9dbbfb2035568f9501f2831491878b051f288bf9
arst <artem.startsev@gmail.com>
2019-12-11 22:11:08 +0200

    Unrolled list

---
+++ AlgorithmsAndDataStructures.Tests/DataStructures/UnrolledLinkedList/UnrolledLinkedListTests.cs
@@ -0,0 +1,42 @@
+﻿using AlgorithmsAndDataStructures.DataStructures.UnrolledLinkedList;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.DataStructures.UnrolledLinkedList
+{
+    public class UnrolledLinkedListTests
+    {
+        [Fact]
+        public void UnrolledLinkedListUnrollsIntoArray()
+        {
+            var sut = new UnrolledLinkedList<int>();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                sut.Add(i);
+            }
+
+            var unrolled = sut.Unroll();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                Assert.Equal(i, unrolled[i]);
+            }
+        }
+
+        [Fact]
+        public void CanFindElements()
+        {
+            var sut = new UnrolledLinkedList<int>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                sut.Add(i);
+            }
+
+            var node = sut.Find(5);
+
+            Assert.Equal(1, node.CurrentIndex);
+            Assert.Equal(5, node.Values[0]);
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/DataStructures/UnrolledLinkedList/UnrolledLinkedListNode.cs
@@ -0,0 +1,11 @@
+﻿namespace AlgorithmsAndDataStructures.DataStructures.UnrolledLinkedList
+{
+    public class UnrolledLinkedListNode<T>
+    {
+        public int CurrentIndex { get; set; }
+
+        public T[] Values { get; set; } = new T[5];
+
+        public UnrolledLinkedListNode<T> Next { get; set; }
+    }
+}
