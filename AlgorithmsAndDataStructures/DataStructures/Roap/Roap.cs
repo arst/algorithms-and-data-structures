77b1f3e4053a76fba5358c0190693078d4ad13fd
arst <artem.startsev@gmail.com>
2020-01-08 22:58:58 +0200

    Rope template

---
+++ AlgorithmsAndDataStructures/DataStructures/Roap/Roap.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace AlgorithmsAndDataStructures.DataStructures.Roap
+{
+    public class Roap
+    {
+        private RoapNode root = new RoapNode();
+
+        public Roap(string start = "")
+        {
+            root.Left = new RoapNode();
+            root.Weight = start.Length;
+            root.Left.Text = start;
+        }
+
+        public void Concat(string input)
+        {
+            var oldRoot = this.root;
+            this.root = new RoapNode();
+            this.root.Weight = oldRoot.Weight;
+            this.root.Left = oldRoot;
+            this.root.Right = new RoapNode()
+            {
+                Text = input,
+                Weight = input.Length,
+            };
+        }
+
+        public char CharAt(int index)
+        {
+            return CharAtInternal(root, index);            
+        }
+
+        public char CharAtInternal(RoapNode node, int index)
+        {
+            if (!string.IsNullOrEmpty(node?.Text))
+            {
+                return node.Text[index];
+            }
+
+            if (node is null || (node.Weight < index && node.Right == null))
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return index < node.Weight ? CharAtInternal(node.Left, index) : CharAtInternal(node.Right, index - root.Weight); 
+        }
+
+        public string Traverse()
+        {
+            return TraverseInternal(root);
+        }
+
+        private string TraverseInternal(RoapNode root)
+        {
+            if (root is null)
+            {
+                return string.Empty;
+            }
+
+            if (!string.IsNullOrEmpty(root.Text))
+            {
+                return root.Text;
+            }
+
+            return TraverseInternal(root.Left) + TraverseInternal(root.Right);
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/DataStructures/Roap/RoapNode.cs
@@ -0,0 +1,13 @@
+﻿namespace AlgorithmsAndDataStructures.DataStructures.Roap
+{
+    public class RoapNode
+    {
+        public int Weight { get; set; }
+
+        public string Text { get; set; }
+
+        public RoapNode Left { get; set; }
+
+        public RoapNode Right { get; set; }
+    }
+}
