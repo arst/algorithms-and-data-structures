a29d457422982974b6d1b60e3d63ee6b50af3305
arst <artem.startsev@gmail.com>
2020-01-27 23:43:24 +0200

    Radix Sort

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Sorting/RadixSortTests.cs
@@ -0,0 +1,15 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Sorting;
+using System;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Sorting
+{
+    public class RadixSortTests : BaseSortingTests
+    {
+        protected override int MaxValue => 1000;
+
+        protected override ISortingAlgorithm GetSystemUnderTest()
+        {
+            return new RadixSort();
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Sorting/RadixSort.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Sorting
+{
+    public class RadixSort : ISortingAlgorithm
+    {
+        public void Sort(int[] target)
+        {
+            if (!target.Any())
+            {
+                return;
+            }
+
+            var max = target.Max();
+
+            for (int exp = 1; max / exp > 0; exp *= 10)
+            {
+                StableCountingSort(target, exp);
+            }
+        }
+
+        private void StableCountingSort(int[] target, int exp)
+        {
+            var max = target.Max();
+            var counters = new int[10];
+            var holder = new int[target.Length];
+
+            for (int i = 0; i < target.Length; i++)
+            {
+                counters[(target[i] / exp) % 10]++;
+            }
+
+            for (int i = 1; i < counters.Length; i++)
+            {
+                counters[i] = counters[i] + counters[i - 1];
+            }
+
+            for (int i = target.Length - 1; i > -1; i--)
+            {
+                counters[(target[i] / exp) % 10]--;
+                holder[counters[(target[i] / exp) % 10]] = target[i];
+            }
+
+            for (int i = 0; i < target.Length; i++)
+            {
+                target[i] = holder[i];
+            }
+        }
+    }
+}
