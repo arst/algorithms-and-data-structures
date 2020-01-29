5efa4693fc71b38585ab3861e9367ad6a585d1de
arst <artem.startsev@gmail.com>
2020-01-29 08:12:43 +0200

    Exponential Search

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Search/ExponentialSearchTests.cs
@@ -0,0 +1,12 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Search;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Search
+{
+    public class ExponentialSearchTests : BaseSearchAlgorithmTests
+    {
+        public override ISearchAlgorithm<int> GetSystemUnderTest()
+        {
+            return new Exponential<int>();
+        }
+    }
+}
