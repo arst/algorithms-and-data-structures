1a9e7836aa2a11393d76cbdbd52f189c2686d56f
arst <artem.startsev@gmail.com>
2020-01-31 09:00:59 +0200

    Depth first search

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/DepthFirstSearchTests.cs
@@ -0,0 +1,71 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph;
+using AlgorithmsAndDataStructures.DataStructures.Graph;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph
+{
+    public class DepthFirstSearchTests
+    {
+        [Fact]
+        public void OneNodeGraph()
+        {
+            var sut = new DepthFirstSearch();
+            var graph = new GraphNode<int>[]
+                {
+                    new GraphNode<int>()
+                    {
+                        Value = 1
+                    },
+                };
+            Assert.Collection(sut.Traverse(graph), arg => Assert.Equal(1, arg));
+        }
+
+        [Fact]
+        public void TwoNodesGraph()
+        {
+            var sut = new DepthFirstSearch();
+            var graph = new GraphNode<int>[]
+                {
+                    new GraphNode<int>()
+                    {
+                        Value = 1,
+                        AdjacentNodes = new System.Collections.Generic.List<int>()
+                        { 
+                            1
+                        }
+                    },
+                    new GraphNode<int>()
+                    {
+                        Value = 2
+                    },
+                };
+            Assert.Collection(sut.Traverse(graph), arg => Assert.Equal(1, arg), arg => Assert.Equal(2, arg));
+        }
+
+        [Fact]
+        public void TwoNodesGraphWithCircularDependency()
+        {
+            var sut = new DepthFirstSearch();
+            var graph = new GraphNode<int>[]
+                {
+                    new GraphNode<int>()
+                    {
+                        Value = 1,
+                        AdjacentNodes = new System.Collections.Generic.List<int>()
+                        {
+                            1
+                        }
+                    },
+                    new GraphNode<int>()
+                    {
+                        Value = 2,
+                        AdjacentNodes = new System.Collections.Generic.List<int>()
+                        {
+                            0
+                        }
+                    },
+                };
+            Assert.Collection(sut.Traverse(graph), arg => Assert.Equal(1, arg), arg => Assert.Equal(2, arg));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/DepthFirstSearch.cs
@@ -0,0 +1,34 @@
+﻿using AlgorithmsAndDataStructures.DataStructures.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph
+{
+    public class DepthFirstSearch
+    {
+        public List<int> Traverse(GraphNode<int>[] graph)
+        {
+            var result = new HashSet<int>();
+            var visited = new HashSet<int>();
+
+            TraverseInternal(0, graph, result, visited);
+            
+            return result.ToList();
+        }
+
+        private void TraverseInternal(int node, GraphNode<int>[] graph, HashSet<int> result, HashSet<int> visited)
+        {
+            result.Add(graph[node].Value);
+            visited.Add(node);
+
+            var notVisitedAdjacentNodes = graph[node].AdjacentNodes
+                .Where(arg => !visited.Contains(arg));
+
+            foreach (var adjacentNode in notVisitedAdjacentNodes)
+            {
+                TraverseInternal(adjacentNode, graph, result, visited);   
+            }
+        }
+    }
+}
