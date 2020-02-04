f52eb4ae1589dbdbff860ab2e46d5ef9e701ada8
arst <artem.startsev@gmail.com>
2020-02-04 22:14:09 +0200

    Cycle Detection in Graph

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/CycleDetectionTests.cs
@@ -0,0 +1,122 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph;
+using AlgorithmsAndDataStructures.DataStructures.Graph;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph
+{
+    public class CycleDetectionTests
+    {
+        [Fact]
+        public void SelfCycleIsDetected()
+        {
+            var sut = new CycleDetection();
+            var graph = new GraphNode<int>[]
+                {
+                    new GraphNode<int>()
+                    {
+                        Value = 1,
+                        AdjacentNodes = new System.Collections.Generic.List<int>() { 0 }
+                    },
+                };
+
+            Assert.True(sut.IsCyclic(graph));
+        }
+
+        [Fact]
+        public void SimpleTwoNodeCycleIsDetected()
+        {
+            var sut = new CycleDetection();
+            var graph = new GraphNode<int>[]
+                {
+                    new GraphNode<int>()
+                    {
+                        Value = 1,
+                        AdjacentNodes = new System.Collections.Generic.List<int>() { 1 }
+                    },
+                    new GraphNode<int>()
+                    {
+                        Value = 2,
+                        AdjacentNodes = new System.Collections.Generic.List<int>() { 0 }
+                    },
+                };
+
+            Assert.True(sut.IsCyclic(graph));
+        }
+
+        [Fact]
+        public void CycleIsNotDetectedForNonCycledGraph()
+        {
+            var sut = new CycleDetection();
+            var graph = new GraphNode<int>[]
+                {
+                    new GraphNode<int>()
+                    {
+                        Value = 1,
+                        AdjacentNodes = new System.Collections.Generic.List<int>() { 1 }
+                    },
+                    new GraphNode<int>()
+                    {
+                        Value = 2,
+                    },
+                };
+
+            Assert.False(sut.IsCyclic(graph));
+        }
+
+        [Fact]
+        public void DisconnectedGraphCycleIsDetected()
+        {
+            var sut = new CycleDetection();
+            var graph = new GraphNode<int>[]
+                {
+                    new GraphNode<int>()
+                    {
+                        Value = 1,
+                    },
+                    new GraphNode<int>()
+                    {
+                        Value = 2,
+                        AdjacentNodes = new System.Collections.Generic.List<int>() { 2 }
+                    },
+                    new GraphNode<int>()
+                    {
+                        Value = 2,
+                        AdjacentNodes = new System.Collections.Generic.List<int>() { 1 }
+                    },
+                };
+
+            Assert.True(sut.IsCyclic(graph));
+        }
+
+        [Fact]
+        public void DisconnectedGraphWithMultipleCyclesIsDetected()
+        {
+            var sut = new CycleDetection();
+            var graph = new GraphNode<int>[]
+                {
+                    new GraphNode<int>()
+                    {
+                        Value = 1,
+                        AdjacentNodes = new System.Collections.Generic.List<int>() { 1, 2 }
+                    },
+                    new GraphNode<int>()
+                    {
+                        Value = 2,
+                        AdjacentNodes = new System.Collections.Generic.List<int>() { 2 }
+                    },
+                    new GraphNode<int>()
+                    {
+                        Value = 2,
+                        AdjacentNodes = new System.Collections.Generic.List<int>() { 0, 3 }
+                    },
+                    new GraphNode<int>()
+                    {
+                        Value = 2,
+                        AdjacentNodes = new System.Collections.Generic.List<int>() { 3 }
+                    },
+                };
+
+            Assert.True(sut.IsCyclic(graph));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/CycleDetection.cs
@@ -0,0 +1,45 @@
+﻿using AlgorithmsAndDataStructures.DataStructures.Graph;
+using System.Collections.Generic;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph
+{
+    public class CycleDetection
+    {
+        public bool IsCyclic(GraphNode<int>[] graph)
+        {
+            var hasCycles = false;
+
+            // We need to iterate over all vertices for diconnected graphs.
+            for (int i = 0; i < graph.Length; i++)
+            {
+                var visited = new bool[graph.Length];
+                visited[i] = true;
+                hasCycles = IsCyclic(graph, i, visited);
+            }
+
+            return hasCycles;
+        }
+
+        private bool IsCyclic(GraphNode<int>[] graph, int node, bool[] visited)
+        {
+            List<int> adjacentNodes = graph[node].AdjacentNodes;
+
+            for (int i = 0; i < adjacentNodes.Count; i++)
+            {
+                if (visited[adjacentNodes[i]])
+                {
+                    return true;
+                }
+                else
+                {
+                    visited[adjacentNodes[i]] = true;
+                    return IsCyclic(graph, adjacentNodes[i], visited);
+                }
+            }
+
+            visited[node] = false;
+
+            return false;
+        }
+    }
+}
