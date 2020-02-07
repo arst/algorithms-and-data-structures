e68c228e07fa96aa8fbcf915bf9b11c726bc8ac6
arst <artem.startsev@gmail.com>
2020-02-07 08:50:25 +0200

    Topological sort

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/TopologicalSortTests.cs
@@ -0,0 +1,117 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph;
+using AlgorithmsAndDataStructures.DataStructures.Graph;
+using System;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph
+{
+    public class TopologicalSortTests
+    {
+        [Fact]
+        public void TopologicalSortForOneVertexGraph()
+        {
+            var sut = new TopologicalSort();
+
+            var node1 = new GraphNode<int>();
+
+            var topologicalOrder = sut.GetTopologicalOrder(new GraphNode<int>[] { node1 });
+
+            Assert.Single(topologicalOrder);
+            Assert.Collection<int>(topologicalOrder, arg => Assert.Equal(0, arg));
+        }
+
+        [Fact]
+        public void TopologicalSortForTwoVertexGraph()
+        {
+            var sut = new TopologicalSort();
+
+            var node1 = new GraphNode<int>()
+            { 
+                AdjacentNodes = new System.Collections.Generic.List<int>() { 1 }
+            };
+
+            var node2 = new GraphNode<int>();
+
+            var topologicalOrder = sut.GetTopologicalOrder(new GraphNode<int>[] { node1, node2 });
+
+            Assert.Equal(2,topologicalOrder.Count);
+            Assert.Collection<int>(topologicalOrder, arg => Assert.Equal(0, arg), arg => Assert.Equal(1, arg));
+        }
+
+        [Fact]
+        public void TopologicalSortForGraphWithCycle()
+        {
+            var sut = new TopologicalSort();
+
+            var node1 = new GraphNode<int>()
+            {
+                AdjacentNodes = new System.Collections.Generic.List<int>() { 1 }
+            };
+
+            var node2 = new GraphNode<int>()
+            {
+                AdjacentNodes = new System.Collections.Generic.List<int>() { 0 }
+            };
+
+            Assert.Throws<ArgumentException>(() => sut.GetTopologicalOrder(new GraphNode<int>[] { node1, node2 }));
+        }
+
+        [Fact]
+        public void TopologicalSortForDisconectedGraph()
+        {
+            var sut = new TopologicalSort();
+
+            var node1 = new GraphNode<int>()
+            {
+                AdjacentNodes = new System.Collections.Generic.List<int>() { 1 }
+            };
+
+            var node2 = new GraphNode<int>();
+
+            var node3 = new GraphNode<int>();
+
+            var topologicalOrder = sut.GetTopologicalOrder(new GraphNode<int>[] { node1, node2, node3 });
+
+            Assert.Equal(3, topologicalOrder.Count);
+            Assert.Collection<int>(topologicalOrder, arg => Assert.Equal(2, arg), arg => Assert.Equal(0, arg), arg => Assert.Equal(1, arg));
+        }
+
+        [Fact]
+        public void Baseline()
+        {
+            var sut = new TopologicalSort();
+
+            var node1 = new GraphNode<int>()
+            {
+                AdjacentNodes = new System.Collections.Generic.List<int>() { 1, 2 }
+            };
+
+            var node2 = new GraphNode<int>();
+
+            var node3 = new GraphNode<int>()
+            {
+                AdjacentNodes = new System.Collections.Generic.List<int>() { 3, 5 }
+            };
+
+            var node4 = new GraphNode<int>()
+            {
+                AdjacentNodes = new System.Collections.Generic.List<int>() { 4 }
+            };
+
+            var node5 = new GraphNode<int>();
+
+            var node6 = new GraphNode<int>();
+
+            var topologicalOrder = sut.GetTopologicalOrder(new GraphNode<int>[] { node1, node2, node3, node4, node5, node6 });
+
+            Assert.Equal(6, topologicalOrder.Count);
+            Assert.Collection<int>(topologicalOrder, 
+                arg => Assert.Equal(0, arg), 
+                arg => Assert.Equal(2, arg), 
+                arg => Assert.Equal(5, arg),
+                arg => Assert.Equal(3, arg),
+                arg => Assert.Equal(4, arg),
+                arg => Assert.Equal(1, arg));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/TopologicalSort.cs
@@ -0,0 +1,46 @@
+﻿using AlgorithmsAndDataStructures.DataStructures.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph
+{
+    public class TopologicalSort
+    {
+        public List<int> GetTopologicalOrder(GraphNode<int>[] graph)
+        {
+            if ((new CycleDetection()).IsCyclic(graph))
+            {
+                throw new ArgumentException("This algorythm only operates on acyclic graphs.");
+            }
+
+            var order = new Stack<int>();
+            var visited = new bool[graph.Length];
+
+            for (int i = 0; i < visited.Length; i++)
+            {
+                if (!visited[i])
+                {
+                    Sort(graph, i, order, visited);
+                }
+            }
+           
+            
+            return order.ToList();
+        }
+
+        private void Sort(GraphNode<int>[] graph, int currentVertex, Stack<int> order, bool[] visited)
+        {
+            for (int i = 0; i < graph[currentVertex].AdjacentNodes.Count; i++)
+            {
+                if (!visited[graph[currentVertex].AdjacentNodes[i]])
+                {
+                    Sort(graph, graph[currentVertex].AdjacentNodes[i], order, visited);
+                }
+            }
+
+            visited[currentVertex] = true;
+            order.Push(currentVertex);
+        }
+    }
+}
