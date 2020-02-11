6a746115366764aba5b9b5017336ec5e18532766
arst <artem.startsev@gmail.com>
2020-02-11 09:25:16 +0200

    Prim's algorithm

---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/PrimsAlgorithm.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph
+{
+    public class PrimsAlgorithm
+    {
+        public int GetMinimumSpanningTreeWeight(WeightedGraphNode[] graph)
+        {
+            var minimumSpanningTreeWeight = 0;
+            var minimumSpanningTree = new List<WeightedGraphNodeEdge>();
+            var spanningTree = new HashSet<int>();
+            var edges = graph.SelectMany(arg => arg.Edges).ToList();
+            spanningTree.Add(0);
+
+            while (spanningTree.Count < graph.Length)
+            {
+                WeightedGraphNodeEdge minEdge = null;
+
+                for (int i = 0; i < edges.Count; i++)
+                {
+                    if ((minEdge == null || minEdge.Weight > edges[i].Weight) && spanningTree.Contains(edges[i].From) && !spanningTree.Contains(edges[i].To))
+                    {
+                        minEdge = edges[i];
+                    }
+                }
+                edges.Remove(minEdge);
+                minimumSpanningTree.Add(minEdge);
+                minimumSpanningTreeWeight += minEdge.Weight;
+                spanningTree.Add(minEdge.To);
+            }
+
+            return minimumSpanningTreeWeight;
+        }
+    }
+}
