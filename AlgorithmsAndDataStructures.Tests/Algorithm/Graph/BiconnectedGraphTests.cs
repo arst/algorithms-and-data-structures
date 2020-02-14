5bf00afa91b2c62d9c72d515cf8be31cd21ca2ed
arst <artem.startsev@gmail.com>
2020-02-14 09:14:19 +0200

    Graph biconnectivity check based on Tarjan's algorithm

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/BiconnectedGraphTests.cs
@@ -0,0 +1,71 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Common;
+using AlgorithmsAndDataStructures.Algorithms.Graph.Misc;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph
+{
+    public class BiconnectedGraphTests
+    {
+        [Fact]
+        public void TriangularGraphIsBiconnected()
+        {
+            var sut = new BiconnectedGraph();
+            var graph = new UndirectedGraph(3);
+            graph.Connect(0, 1);
+            graph.Connect(1, 2);
+            graph.Connect(2, 0);
+
+            Assert.True(sut.IsBiconnected(graph));
+        }
+
+        [Fact]
+        public void LinedUpGraphIsNotBiconnected()
+        {
+            var sut = new BiconnectedGraph();
+            var graph = new UndirectedGraph(3);
+            graph.Connect(0, 1);
+            graph.Connect(1, 2);
+
+            Assert.False(sut.IsBiconnected(graph));
+        }
+
+        [Fact]
+        public void TwoVerticesGraphIsBiconnected()
+        {
+            var sut = new BiconnectedGraph();
+            var graph = new UndirectedGraph(2);
+            graph.Connect(0, 1);
+
+            Assert.True(sut.IsBiconnected(graph));
+        }
+
+        [Fact]
+        public void DenseGraphIsBiconnected()
+        {
+            var sut = new BiconnectedGraph();
+            var graph = new UndirectedGraph(5);
+            graph.Connect(1, 0);
+            graph.Connect(0, 2);
+            graph.Connect(2, 1);
+            graph.Connect(0, 3);
+            graph.Connect(3, 4);
+            graph.Connect(2, 4);
+
+            Assert.True(sut.IsBiconnected(graph));
+        }
+
+        [Fact]
+        public void DenseGraphWithoutBackedgeIsNotBiconnected()
+        {
+            var sut = new BiconnectedGraph();
+            var graph = new UndirectedGraph(5);
+            graph.Connect(1, 0);
+            graph.Connect(0, 2);
+            graph.Connect(2, 1);
+            graph.Connect(0, 3);
+            graph.Connect(3, 4);
+
+            Assert.False(sut.IsBiconnected(graph));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/Misc/BiconnectedGraph.cs
@@ -0,0 +1,66 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph.Misc
+{
+    public class BiconnectedGraph
+    {
+        private const int nullParent = -1;
+
+        public bool IsBiconnected(UndirectedGraph graph)
+        {
+            var vertices = graph.Vertices();
+            var visited = new bool[vertices.Length];
+            var parents = new int[vertices.Length];
+            var discoveryTimes = new int[vertices.Length];
+            var lowestReachableDiscoveryTime = new int[vertices.Length];
+
+            parents[0] = nullParent;
+            var result = IsBiconnected(vertices, 0, 0, visited, parents, discoveryTimes, lowestReachableDiscoveryTime) && visited.All(arg => arg);
+
+            return result;
+        }
+
+        private bool IsBiconnected(List<int>[] vertices, int currentVertice, int currentDiscoveryTime, bool[] visited, int[] parents, int[] discoveryTimes, int[] lowestReachableDiscoveryTime)
+        {
+            visited[currentVertice] = true;
+            discoveryTimes[currentVertice] = lowestReachableDiscoveryTime[currentVertice] = currentDiscoveryTime++;
+            var children = 0;
+
+            foreach (var adjacentVertice in vertices[currentVertice])
+            {
+                if (!visited[adjacentVertice])
+                {
+                    children++;
+                    parents[adjacentVertice] = currentVertice;
+
+                    var result = IsBiconnected(vertices, adjacentVertice, currentDiscoveryTime, visited, parents, discoveryTimes, lowestReachableDiscoveryTime);
+                    
+                    if (!result)
+                    {
+                        return result;
+                    }
+
+                    lowestReachableDiscoveryTime[currentVertice] = Math.Min(lowestReachableDiscoveryTime[currentVertice], lowestReachableDiscoveryTime[adjacentVertice]);
+
+                    if (parents[currentVertice] == nullParent && children > 1)
+                    {
+                        return false;
+                    }
+                    if (parents[currentVertice] != nullParent && lowestReachableDiscoveryTime[adjacentVertice] >= discoveryTimes[currentVertice])
+                    {
+                        return false;
+                    }
+                }
+                else if (adjacentVertice != parents[currentVertice])
+                {
+                    lowestReachableDiscoveryTime[currentVertice] = Math.Min(lowestReachableDiscoveryTime[currentVertice], discoveryTimes[adjacentVertice]);
+                }
+            }
+
+            return true;
+        }
+    }
+}
