232cad765dadad51987dddc1677d099ddeda6aab
arst <artem.startsev@gmail.com>
2020-02-18 09:03:09 +0200

    Tarjan's algorithm for biconnected components

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/TarjansAlgorithmForBiconnectedComponentsTests.cs
@@ -0,0 +1,74 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Misc;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph
+{
+    public class TarjansAlgorithmForBiconnectedComponentsTests
+    {
+        [Fact]
+        public void EmptyGraphHashNoBiconnectedComponents()
+        {
+            var sut = new TarjansAlgorithmForBiconnectedComponents();
+            var biconnectedComponents = sut.GetBiconnectedComponents(new Algorithms.Graph.Common.UndirectedGraph(0));
+            Assert.Empty(biconnectedComponents);
+        }
+
+        [Fact]
+        public void OneVerticeGraphHashOneBiconnectedComponent()
+        {
+            var sut = new TarjansAlgorithmForBiconnectedComponents();
+            var biconnectedComponents = sut.GetBiconnectedComponents(new Algorithms.Graph.Common.UndirectedGraph(1));
+            Assert.Collection(biconnectedComponents, arg => {
+                Assert.Collection(arg, arg => Assert.Equal(0, arg));
+            });
+        }
+
+        [Fact]
+        public void TwoVerticeGraphHashHashOneBiconnectedComponent()
+        {
+            var sut = new TarjansAlgorithmForBiconnectedComponents();
+            Algorithms.Graph.Common.UndirectedGraph graph = new Algorithms.Graph.Common.UndirectedGraph(2);
+            graph.Connect(0, 1);
+            var biconnectedComponents = sut.GetBiconnectedComponents(graph);
+            Assert.Collection(biconnectedComponents, arg => {
+                Assert.Collection(arg, arg => Assert.Equal(1, arg), arg => Assert.Equal(0, arg));
+            });
+        }
+
+        [Fact]
+        public void ThreeVerticeGraphHashHashTwoBiconnectedComponent()
+        {
+            var sut = new TarjansAlgorithmForBiconnectedComponents();
+            Algorithms.Graph.Common.UndirectedGraph graph = new Algorithms.Graph.Common.UndirectedGraph(3);
+            graph.Connect(0, 1);
+            graph.Connect(1, 2);
+            var biconnectedComponents = sut.GetBiconnectedComponents(graph);
+            Assert.Collection(biconnectedComponents, arg => {
+                Assert.Collection(arg, arg => Assert.Equal(1, arg), arg => Assert.Equal(2, arg));
+            },
+             arg => {
+                 Assert.Collection(arg, arg => Assert.Equal(1, arg), arg => Assert.Equal(0, arg));
+             });
+        }
+
+        [Fact]
+        public void LinedUpGraph()
+        {
+            var sut = new TarjansAlgorithmForBiconnectedComponents();
+            Algorithms.Graph.Common.UndirectedGraph graph = new Algorithms.Graph.Common.UndirectedGraph(4);
+            graph.Connect(0, 1);
+            graph.Connect(1, 2);
+            graph.Connect(2, 3);
+            var biconnectedComponents = sut.GetBiconnectedComponents(graph);
+            Assert.Collection(biconnectedComponents, arg => {
+                Assert.Collection(arg, arg => Assert.Equal(2, arg), arg => Assert.Equal(3, arg));
+            },
+             arg => {
+                 Assert.Collection(arg, arg => Assert.Equal(1, arg), arg => Assert.Equal(2, arg));
+             },
+             arg => {
+                 Assert.Collection(arg, arg => Assert.Equal(1, arg), arg => Assert.Equal(0, arg));
+             });
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/Misc/TarjansAlgorithmForBiconnectedComponents.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmsAndDataStructures.Algorithms.Graph.Common;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph.Misc
+{
+    public class TarjansAlgorithmForBiconnectedComponents
+    {
+        private const int nullParent = -1;
+
+        public List<List<int>> GetBiconnectedComponents(UndirectedGraph graph)
+        {
+            var vertices = graph.Vertices();
+
+            if (vertices.Length == 0)
+            {
+                return new List<List<int>>();
+            }
+
+            var visited = new bool[vertices.Length];
+            var parents = new int[vertices.Length];
+            var dicoveryTime = new int[vertices.Length];
+            var lowestSubTreeDiscoveryTime = new int[vertices.Length];
+            var dfs = new Stack<int>(vertices.Length);
+            var biconnectedComponents = new List<List<int>>();
+
+            parents[0] = nullParent;
+
+            DFSArticulationTraversal(vertices, 0, visited, parents, dicoveryTime, lowestSubTreeDiscoveryTime, dfs, biconnectedComponents, 0);
+
+            if (dfs.Any())
+            {
+                var component = new List<int>();
+
+                while (dfs.Any())
+                {
+                    component.Add(dfs.Pop());
+                }
+
+                biconnectedComponents.Add(component);
+            }
+
+            return biconnectedComponents;
+        }
+
+        private void DFSArticulationTraversal(
+            List<int>[] vertices,
+            int currentVertice,
+            bool[] visited, 
+            int[] parents, 
+            int[] dicoveryTime, 
+            int[] lowestSubTreeDiscoveryTime,
+            Stack<int> dfs,
+            List<List<int>> biconnectedComponents,
+            int time)
+        {
+            visited[currentVertice] = true;
+            var children = 0;
+            int currentDiscoveryTime = time++;
+            dicoveryTime[currentVertice] = currentDiscoveryTime;
+            lowestSubTreeDiscoveryTime[currentVertice] = currentDiscoveryTime;
+            dfs.Push(currentVertice);
+
+            foreach (var adjacentVertice in vertices[currentVertice])
+            {
+                if (!visited[adjacentVertice])
+                {
+                    children++;
+                    parents[adjacentVertice] = currentVertice;
+                    DFSArticulationTraversal(
+                        vertices,
+                        adjacentVertice,
+                        visited,
+                        parents,
+                        dicoveryTime,
+                        lowestSubTreeDiscoveryTime,
+                        dfs,
+                        biconnectedComponents,
+                        time);
+
+                    lowestSubTreeDiscoveryTime[currentVertice] = Math.Min(lowestSubTreeDiscoveryTime[currentVertice], lowestSubTreeDiscoveryTime[adjacentVertice]);
+
+                    var isActiculationPoint = false;
+
+                    if (parents[currentVertice] == nullParent && children > 1)
+                    {
+                        isActiculationPoint = true;
+                        
+                    }
+                    if (parents[currentVertice] != nullParent && lowestSubTreeDiscoveryTime[adjacentVertice] >= dicoveryTime[currentVertice])
+                    {
+                        isActiculationPoint = true;
+                    }
+
+                    if (isActiculationPoint)
+                    {
+                        var component = new List<int>();
+                        component.Add(currentVertice);
+
+                        while (dfs.Peek() != currentVertice)
+                        {
+                            component.Add(dfs.Pop());
+                        }
+
+                        biconnectedComponents.Add(component);
+                    }
+                }
+                else if(adjacentVertice != parents[currentVertice])
+                {
+                    lowestSubTreeDiscoveryTime[currentVertice] = Math.Min(lowestSubTreeDiscoveryTime[currentVertice], dicoveryTime[adjacentVertice]);
+                }
+            }
+        }
+    }
+}
