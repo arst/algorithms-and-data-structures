ed7180245d6ca75c4128c1f1516b648747969d12
arst <artem.startsev@gmail.com>
2020-02-18 09:31:18 +0200

    Bridges in a grpah based on Trojan's algorithm

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/BridgesInGraphTests.cs
@@ -0,0 +1,61 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Common;
+using AlgorithmsAndDataStructures.Algorithms.Graph.Misc;
+using System;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph
+{
+    public class BridgesInGraphTests
+    {
+        [Fact]
+        public void EmptyGraphHasNoBridges() 
+        {
+            var sut = new BridgesInGraph();
+            var graph = new UndirectedGraph(0);
+
+            Assert.Empty(sut.GetBridges(graph));
+        }
+
+        [Fact]
+        public void OneVerticeGraphHasNoBridges()
+        {
+            var sut = new BridgesInGraph();
+            var graph = new UndirectedGraph(1);
+
+            Assert.Empty(sut.GetBridges(graph));
+        }
+
+        [Fact]
+        public void TwoVerticeGraphHasOneBridges()
+        {
+            var sut = new BridgesInGraph();
+            var graph = new UndirectedGraph(2);
+            graph.Connect(0,1);
+
+            Assert.Collection(sut.GetBridges(graph), arg => Assert.Equal(new Tuple<int, int>(0,1), arg));
+        }
+
+        [Fact]
+        public void ThreeVerticeGraphHasNoBridges()
+        {
+            var sut = new BridgesInGraph();
+            var graph = new UndirectedGraph(3);
+            graph.Connect(0, 1);
+            graph.Connect(1, 2);
+            graph.Connect(2, 0);
+
+            Assert.Empty(sut.GetBridges(graph));
+        }
+
+        [Fact]
+        public void ThreeVerticeGraphHasTwoBridges()
+        {
+            var sut = new BridgesInGraph();
+            var graph = new UndirectedGraph(3);
+            graph.Connect(0, 1);
+            graph.Connect(0, 2);
+
+            Assert.Collection(sut.GetBridges(graph), arg => Assert.Equal(new Tuple<int, int>(0, 1), arg), arg => Assert.Equal(new Tuple<int, int>(0, 2), arg));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/Misc/BridgesInGraph.cs
@@ -0,0 +1,62 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Common;
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph.Misc
+{
+    public class BridgesInGraph
+    {
+        private const int nullParent = -1;
+
+        public List<Tuple<int, int>> GetBridges(UndirectedGraph grpah)
+        {
+            var vertices = grpah.Vertices();
+            var discoveryTimes = new int[vertices.Length];
+            var lowestReachableDicoveryTimeInSubtree = new int[vertices.Length];
+            var visited = new bool[vertices.Length];
+            var bridges = new List<Tuple<int, int>>();
+            var parents = new int[vertices.Length];
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                if (!visited[i])
+                    GetBridges(i, 0, vertices, discoveryTimes, lowestReachableDicoveryTimeInSubtree, visited, parents, bridges);
+            }
+
+            return bridges;
+        }
+
+        private void GetBridges(int currentVertice, int dicoveryTime, List<int>[] vertices, int[] discoveryTimes, int[] lowestReachableDicoveryTimeInSubtree, bool[] visited, int[] parents, List<Tuple<int, int>> bridges)
+        {
+            discoveryTimes[currentVertice] = lowestReachableDicoveryTimeInSubtree[currentVertice] = dicoveryTime++;
+            visited[currentVertice] = true;
+            var children = 0;
+
+            foreach (var adjacentVertice in vertices[currentVertice])
+            {
+                if (!visited[adjacentVertice])
+                {
+                    children++;
+                    parents[adjacentVertice] = currentVertice;
+                    GetBridges(adjacentVertice, dicoveryTime, vertices, discoveryTimes, lowestReachableDicoveryTimeInSubtree, visited, parents, bridges);
+
+                    lowestReachableDicoveryTimeInSubtree[currentVertice] = Math.Min(lowestReachableDicoveryTimeInSubtree[currentVertice], lowestReachableDicoveryTimeInSubtree[adjacentVertice]);
+
+                    if (parents[currentVertice] == nullParent && children > 1)
+                    {
+                        bridges.Add(new Tuple<int, int>(currentVertice, adjacentVertice));
+                    }
+                    if (parents[currentVertice] != nullParent && lowestReachableDicoveryTimeInSubtree[adjacentVertice] > discoveryTimes[currentVertice])
+                    {
+                        bridges.Add(new Tuple<int, int>(currentVertice, adjacentVertice));
+                    }
+                }
+                else if (adjacentVertice != parents[currentVertice])
+                {
+                    lowestReachableDicoveryTimeInSubtree[currentVertice] = Math.Min(lowestReachableDicoveryTimeInSubtree[currentVertice], discoveryTimes[adjacentVertice]);
+                }
+
+            }
+        }
+    }
+}
