6479eb22cc39765a059a0bac010857b17366701e
arst <artem.startsev@gmail.com>
2020-02-18 21:09:55 +0200

    Eulerian path in  a graph

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/EulerianPathTests.cs
@@ -0,0 +1,114 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Common;
+using AlgorithmsAndDataStructures.Algorithms.Graph.Misc;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph
+{
+    public class EulerianPathTests
+    {
+        [Fact]
+        public void EmptyGraphIsEulerian() 
+        {
+            var sut = new EulerianPath();
+            var graph = new UndirectedGraph(0);
+            var result = sut.IsEulerian(graph);
+
+            Assert.True(result.hasEulerianCycle);
+        }
+
+        [Fact]
+        public void OneVerticeGraphIsEulerian()
+        {
+            var sut = new EulerianPath();
+            var graph = new UndirectedGraph(1);
+            var result = sut.IsEulerian(graph);
+
+            Assert.True(result.hasEulerianCycle);
+        }
+
+        [Fact]
+        public void TwoVerticeGraphIsSemiEulerian()
+        {
+            var sut = new EulerianPath();
+            var graph = new UndirectedGraph(2);
+            graph.Connect(0,1);
+            var result = sut.IsEulerian(graph);
+
+            Assert.False(result.hasEulerianCycle);
+            Assert.True(result.hasEulerianPath);
+        }
+
+        [Fact]
+        public void ThreeVerticeGraphIsEulerian()
+        {
+            var sut = new EulerianPath();
+            var graph = new UndirectedGraph(3);
+            graph.Connect(0, 1);
+            graph.Connect(1, 2);
+            graph.Connect(2, 0);
+            var result = sut.IsEulerian(graph);
+
+            Assert.True(result.hasEulerianCycle);
+        }
+
+        [Fact]
+        public void SemiEulerianGraph()
+        {
+            var sut = new EulerianPath();
+            var graph = new UndirectedGraph(5);
+            graph.Connect(1, 0);
+            graph.Connect(0, 2);
+            graph.Connect(2, 1);
+            graph.Connect(0, 3);
+            graph.Connect(3, 4);
+            var result = sut.IsEulerian(graph);
+
+            Assert.False(result.hasEulerianCycle);
+            Assert.True(result.hasEulerianPath);
+        }
+
+        [Fact]
+        public void FullEulerianGraph()
+        {
+            var sut = new EulerianPath();
+            var graph = new UndirectedGraph(5);
+            graph.Connect(1, 0);
+            graph.Connect(0, 2);
+            graph.Connect(2, 1);
+            graph.Connect(0, 3);
+            graph.Connect(3, 4);
+            graph.Connect(4, 0);
+            var result = sut.IsEulerian(graph);
+
+            Assert.True(result.hasEulerianCycle);
+        }
+
+        [Fact]
+        public void NonEulerianGraph()
+        {
+            var sut = new EulerianPath();
+            var graph = new UndirectedGraph(5);
+            graph.Connect(1, 0);
+            graph.Connect(0, 2);
+            graph.Connect(2, 1);
+            graph.Connect(0, 3);
+            graph.Connect(3, 4);
+            graph.Connect(1, 3);
+            var result = sut.IsEulerian(graph);
+
+            Assert.False(result.hasEulerianCycle);
+            Assert.False(result.hasEulerianPath);
+        }
+
+        [Fact]
+        public void NonConnectedGraphIsNonEulerian()
+        {
+            var sut = new EulerianPath();
+            var graph = new UndirectedGraph(5);
+            var result = sut.IsEulerian(graph);
+
+            Assert.False(result.hasEulerianCycle);
+            Assert.False(result.hasEulerianPath);
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/Misc/EulerianPath.cs
@@ -0,0 +1,80 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Common;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph.Misc
+{
+    public class EulerianPath
+    {
+        public (bool hasEulerianCycle, bool hasEulerianPath) IsEulerian(UndirectedGraph graph)
+        {
+            if (graph.Vertices().Length < 2)
+            {
+                return (true, true);
+            }
+
+            var vertices = graph.Vertices();
+            var visited = new bool[vertices.Length];
+
+
+            // Check if all non-zero degree vertices are connected
+            int nonZeroDegreeVertice = 0;
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                // We need to start from non-zero degree vertice to traverse non-zero degree vertices
+                if (vertices[i].Count > 0)
+                {
+                    nonZeroDegreeVertice = i;
+                    break;
+                }
+                //There is no non-zero degree vertices, graph is disconnected
+                if (i == vertices.Length - 1)
+                {
+                    return (false, false);
+                }
+            }
+
+            DFS(vertices, visited, nonZeroDegreeVertice);
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                // If some of the vertices with non-zero degree are not visited during DFS, then graph is not Eulerian
+                if (!visited[i] && vertices[i].Count > 0)
+                {
+                    return (false, false);
+                }
+            }
+
+            //Count vertices with odd degree, in Eulerian graph there are 0 or 2 vertices with odd degree. It is impossible to have 1 odd degree vertice in undirected graph.
+            var odd = 0;
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                if (vertices[i].Count % 2 != 0)
+                {
+                    odd++;
+                }
+            }
+
+            if (odd > 2)
+            {
+                return (false, false);
+            }
+
+            // if there are 2 vertices with odd degree then it is a semi-eulerian(has only Eulerian path) graph, if all vertices are even then it is a full-eulerian(has Eulerian cycle) graph
+            return (odd == 0, odd == 2);
+        }
+
+        private static void DFS(System.Collections.Generic.List<int>[] vertices, bool[] visited, int currentVertice)
+        {
+            visited[currentVertice] = true;
+
+            for (int i = 0; i < vertices[currentVertice].Count; i++)
+            {
+                if (!visited[vertices[currentVertice][i]])
+                {
+                    DFS(vertices, visited, vertices[currentVertice][i]);
+                }
+            }
+        }
+    }
+}
