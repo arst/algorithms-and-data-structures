87a7290bf94a268cd29e9ed27ae3130159eb117a
arst <artem.startsev@gmail.com>
2020-02-19 09:30:07 +0200

    Transitive Closure Floyd Warshall: template

---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/Misc/TransitiveClosureFloydWarshall.cs
@@ -0,0 +1,29 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Common;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph.Misc
+{
+    public class TransitiveClosureFloydWarshall
+    {
+        public int[][] GetReachabilityMatrix(WeightedGraphNode[] graph)
+        {
+            var reachabilityMatrix = new bool[graph.Length][];
+
+            for (int i = 0; i < reachabilityMatrix.Length; i++)
+            {
+                reachabilityMatrix[i] = new bool[graph.Length];
+            }
+
+            
+            for (int i = 0; i < graph.Length; i++)
+            {
+                for (int j = 0; j < graph.Length; j++)
+                {
+                    for (int k = 0; k < graph.Length; k++)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
