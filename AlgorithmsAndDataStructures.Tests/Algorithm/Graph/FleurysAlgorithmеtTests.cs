0aee4f280153fe493851295412f338b459fb0f28
arst <artem.startsev@gmail.com>
2020-02-19 08:40:21 +0200

    Fleurys algorithm for Eulerian Trail

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/FleurysAlgorithmеtTests.cs
@@ -0,0 +1,93 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Common;
+using AlgorithmsAndDataStructures.Algorithms.Graph.Misc;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph
+{
+    public class FleurysAlgorithmеtTests
+    {
+        [Fact]
+        public void EmptyGraphHasEmptyEulerianTrail()
+        {
+            var sut = new FleurysAlgorithm();
+            var graph = new UndirectedGraph(0);
+
+            Assert.Empty(sut.GetEulerianTrail(graph));
+        }
+
+        [Fact]
+        public void OneVerticeGraphHasEmptyEulerianTrail()
+        {
+            var sut = new FleurysAlgorithm();
+            var graph = new UndirectedGraph(1);
+
+            Assert.Empty(sut.GetEulerianTrail(graph));
+        }
+
+        [Fact]
+        public void TwoVerticeGraphHasOneEdgeEulerianTrail()
+        {
+            var sut = new FleurysAlgorithm();
+            var graph = new UndirectedGraph(2);
+            graph.Connect(0, 1);
+
+            Assert.Collection(sut.GetEulerianTrail(graph), arg =>
+            {
+                Assert.Equal(1, arg.Item1);
+                Assert.Equal(0, arg.Item2);
+            });
+        }
+
+        [Fact]
+        public void ThreeVerticeGraphHasOneEdgeEulerianTrail()
+        {
+            var sut = new FleurysAlgorithm();
+            var graph = new UndirectedGraph(3);
+            graph.Connect(0, 1);
+            graph.Connect(1, 2);
+
+            Assert.Collection(sut.GetEulerianTrail(graph), arg =>
+            {
+                Assert.Equal(2, arg.Item1);
+                Assert.Equal(1, arg.Item2);
+            },
+            arg =>
+            {
+                Assert.Equal(1, arg.Item1);
+                Assert.Equal(0, arg.Item2);
+            });
+        }
+
+        [Fact]
+        public void BaseLine()
+        {
+            var sut = new FleurysAlgorithm();
+            var graph = new UndirectedGraph(4);
+            graph.Connect(0, 1);
+            graph.Connect(0, 2);
+            graph.Connect(1, 2);
+            graph.Connect(2, 3);
+
+            Assert.Collection(sut.GetEulerianTrail(graph), arg =>
+            {
+                Assert.Equal(3, arg.Item1);
+                Assert.Equal(2, arg.Item2);
+            },
+            arg =>
+            {
+                Assert.Equal(2, arg.Item1);
+                Assert.Equal(1, arg.Item2);
+            },
+            arg =>
+            {
+                Assert.Equal(1, arg.Item1);
+                Assert.Equal(0, arg.Item2);
+            },
+            arg =>
+            {
+                Assert.Equal(0, arg.Item1);
+                Assert.Equal(2, arg.Item2);
+            });
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/Misc/FleurysAlgorithm.cs
@@ -0,0 +1,153 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Common;
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph.Misc
+{
+    public class FleurysAlgorithm
+    {
+        public List<Tuple<int, int>> GetEulerianTrail(UndirectedGraph graph)
+        {
+            var result = new List<Tuple<int, int>>();
+
+            if (graph.Vertices().Length < 2)
+            {
+                return result;
+            }
+
+            var vertices = graph.Vertices();
+            var visited = new bool[vertices.Length];
+
+
+            // Check if all non-zero degree vertices are connected
+            int nonZeroDegreeVertice = 0;
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                // We need to start from non-zero degree vertice to traverse non-zero degree vertices
+                if (vertices[i].Count > 0)
+                {
+                    nonZeroDegreeVertice = i;
+                    break;
+                }
+                //There is no non-zero degree vertices, graph is disconnected
+                if (i == vertices.Length - 1)
+                {
+                    return result;
+                }
+            }
+
+            DFS(vertices, visited, nonZeroDegreeVertice);
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                // If some of the vertices with non-zero degree are not visited during DFS, then graph is not Eulerian
+                if (!visited[i] && vertices[i].Count > 0)
+                {
+                    return result;
+                }
+            }
+
+            //Count vertices with odd degree, in Eulerian graph there are 0 or 2 vertices with odd degree. It is impossible to have 1 odd degree vertice in undirected graph.
+            var oddVerticesCount = 0;
+            var oddVertice = 0;
+
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                if (vertices[i].Count % 2 != 0)
+                {
+                    oddVerticesCount++;
+                    oddVertice = i;
+                }
+            }
+
+            if (oddVerticesCount > 2)
+            {
+                return result;
+            }
+
+            EulerianTraversal(oddVertice, vertices, result);
+
+            return result;
+        }
+
+        private void EulerianTraversal(int currentVertice, List<int>[] vertices, List<Tuple<int, int>> eulerianPath)
+        {
+            var adjacentVertices = vertices[currentVertice];
+
+            for (int i = 0; i < vertices[currentVertice].Count; i++)
+            {
+                if (adjacentVertices[i] == -1)
+                {
+                    continue;
+                }
+
+                if (IsValidEdgeToIncludeInEulerianTrail(currentVertice, adjacentVertices[i], vertices))
+                {
+                    int adjacentVertice = adjacentVertices[i];
+                    eulerianPath.Add(new Tuple<int, int>(currentVertice, adjacentVertice));
+                    RemoveEdge(vertices, currentVertice, adjacentVertice);
+                    EulerianTraversal(adjacentVertice, vertices, eulerianPath);
+                }
+            }
+        }
+
+        private bool IsValidEdgeToIncludeInEulerianTrail(int from, int to, List<int>[] vertices)
+        {
+
+            //if it is the nly edge we have no other choises then to take it
+            if (vertices[from].Count == 1)
+            {
+                return true;
+            }
+
+            var visited = new bool[vertices.Length];
+            var reachableVerticesCount = DFSCount(from, vertices, visited);
+            RemoveEdge(vertices, from, to);
+
+            var visitedReduced = new bool[vertices.Length];
+            var reachableVerticesCountReduced = DFSCount(from, vertices, visitedReduced);
+
+            //add removed edge back to the graph
+            vertices[from].Add(to);
+            vertices[to].Add(from);
+            //if dfs could reach less vertices after node removal then we have a bridge and according to Flurys algorithm we should prefer non-bridges over bridges
+            return reachableVerticesCountReduced > reachableVerticesCount ? false : true;
+        }
+
+        private int DFSCount(int currentVertice, List<int>[] vertices, bool[] visited)
+        {
+            visited[currentVertice] = true;
+            int count = 1;
+
+            for (int i = 0; i < vertices[currentVertice].Count; i++)
+            {
+                if (!visited[vertices[currentVertice][i]])
+                {
+                    return count + DFSCount(vertices[currentVertice][i], vertices, visited);
+                }
+            }
+
+            return count;
+        }
+
+        private static void RemoveEdge(List<int>[] vertices, int from, int to)
+        {
+            var fromIndex = vertices[from].Remove(to);
+            var toIndex = vertices[to].Remove(from);
+        }
+
+        private static void DFS(List<int>[] vertices, bool[] visited, int currentVertice)
+        {
+            visited[currentVertice] = true;
+
+            for (int i = 0; i < vertices[currentVertice].Count; i++)
+            {
+                if (!visited[vertices[currentVertice][i]])
+                {
+                    DFS(vertices, visited, vertices[currentVertice][i]);
+                }
+            }
+        }
+    }
+}
