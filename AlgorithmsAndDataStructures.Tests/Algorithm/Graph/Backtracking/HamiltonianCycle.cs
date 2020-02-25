638e225386218f17933bcaabbd0aa59357711f8c
arst <artem.startsev@gmail.com>
2020-02-25 08:27:51 +0200

    Hamiltonians cycle detection using backtracking

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/Backtracking/HamiltonianCycle.cs
@@ -0,0 +1,70 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Backtracking;
+using AlgorithmsAndDataStructures.Algorithms.Graph.Common;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph.Backtracking
+{
+    public class HamiltonianCycleTest
+    {
+        [Fact]
+        public void Baseline()
+        {
+            var sut = new HamiltonPath();
+            var graph = new UndirectedGraph(5);
+            graph.Connect(0, 1);
+            graph.Connect(0, 3);
+            graph.Connect(1, 2);
+            graph.Connect(1, 3);
+            graph.Connect(1, 4);
+            graph.Connect(2, 4);
+            graph.Connect(3, 4);
+
+            var result = sut.HasHamiltonPath(graph);
+
+            Assert.True(result.hasCycle);
+            Assert.True(result.hasPath);
+            Assert.Collection(result.path, 
+                arg => Assert.Equal(0, arg),
+                arg => Assert.Equal(1, arg),
+                arg => Assert.Equal(2, arg),
+                arg => Assert.Equal(4, arg),
+                arg => Assert.Equal(3, arg),
+                arg => Assert.Equal(0, arg));
+        }
+
+        [Fact]
+        public void BaselineWithNoCycle()
+        {
+            var sut = new HamiltonPath();
+            var graph = new UndirectedGraph(5);
+            graph.Connect(0, 1);
+            graph.Connect(0, 3);
+            graph.Connect(1, 2);
+            graph.Connect(1, 3);
+            graph.Connect(1, 4);
+            graph.Connect(2, 4);
+
+            var result = sut.HasHamiltonPath(graph);
+
+            Assert.False(result.hasCycle);
+            Assert.True(result.hasPath);
+        }
+
+        [Fact]
+        public void BaselineWithNoPath()
+        {
+            var sut = new HamiltonPath();
+            var graph = new UndirectedGraph(5);
+            graph.Connect(0, 1);
+            graph.Connect(0, 3);
+            graph.Connect(1, 2);
+            graph.Connect(1, 4);
+            graph.Connect(2, 4);
+
+            var result = sut.HasHamiltonPath(graph);
+
+            Assert.False(result.hasCycle);
+            Assert.False(result.hasPath);
+        }
+    }
+}
--- AlgorithmsAndDataStructures/Algorithms/Graph/Backtracking/HamiltonPath.cs
+++ AlgorithmsAndDataStructures/Algorithms/Graph/Backtracking/HamiltonPath.cs
@@ -1,12 +1,92 @@
 ﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Common;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgorithmsAndDataStructures.Algorithms.Graph.Backtracking
 {
     public class HamiltonPath
     {
-        public bool HasHamiltonPath(UndirectedGraph graph) 
+        public (bool hasPath, bool hasCycle, int[] path) HasHamiltonPath(UndirectedGraph graph)
         {
+            var vertices = graph.Vertices();
+            var visited = new bool[vertices.Length];
+            var parent = new int[vertices.Length];
+            var path = new int[vertices.Length + 1];
+
+            for (int i = 0; i < parent.Length; i++)
+            {
+                parent[i] = -1;
+            }
+
+            var hasPath = IsHamiltonPath(0, vertices, visited, parent);
+            var hasCycle = hasPath &&HasCycle(parent, vertices);
+
+            if (hasPath)
+            {
+                var index = 0;
+                var pathIndex = 1;
+
+                while (parent[index] != -1)
+                {
+                    path[pathIndex] = parent[index];
+                    pathIndex++;
+                    index = parent[index];
+                }
+
+                path[pathIndex] = hasCycle ? 0 : -1;
+            }
+
+            return (hasPath, hasCycle, path);
+        }
+
+        private bool HasCycle(int[] parent, List<int>[] vertices)
+        {
+            var lastVerticeIndex = -1;
+
+            for (int i = 0; i < parent.Length && lastVerticeIndex < 0; i++)
+            {
+                if (parent[i] == -1)
+                {
+                    lastVerticeIndex = i;
+                    break;
+                }
+            }
+
+            return vertices[lastVerticeIndex].Any(arg => arg == 0);
+        }
+
+        private bool IsHamiltonPath(int currentVertice, List<int>[] vertices, bool[] visited, int[] parent)
+        {
+            visited[currentVertice] = true;
+
+            if (IsPathFormed(visited))
+            {
+                return true;
+            }
+
+            foreach (var adjacentVertices in vertices[currentVertice])
+            {
+                if (!visited[adjacentVertices])
+                {
+                    parent[currentVertice] = adjacentVertices;
+                    var isPathFormed = IsHamiltonPath(adjacentVertices, vertices, visited, parent);
+
+                    if (isPathFormed)
+                    {
+                        return true;
+                    }
+
+                    parent[currentVertice] = -1;
+                    visited[adjacentVertices] = false;
+                }
+            }
+
             return false;
+        }
+
+        private bool IsPathFormed(bool[] visited)
+        {
+            return visited.All(arg => arg);
         }
     }
 }
