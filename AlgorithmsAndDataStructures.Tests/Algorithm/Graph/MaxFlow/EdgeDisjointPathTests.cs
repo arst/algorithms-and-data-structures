819aaa2c51c0775037d9432bb0f1a4c87e9e2fa2
arst <artem.startsev@gmail.com>
2020-02-26 21:37:33 +0200

    Maximum number of edge disjoint paths between two vertices(Fold-Fulkerson)

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/MaxFlow/EdgeDisjointPathTests.cs
@@ -0,0 +1,117 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.MaximumFlow;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph.MaxFlow
+{
+    public class EdgeDisjointPathTests
+    {
+        [Fact]
+        public void NoPaths()
+        {
+            var sut = new EdgeDisjointPath();
+            var graph = new int[4][];
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                graph[i] = new int[4];
+            }
+
+            graph[1][2] = 1;
+            graph[1][3] = 1;
+            graph[2][3] = 1;
+
+            Assert.Equal(0, sut.GetEdgeDisjointPathCount(graph));
+        }
+
+        [Fact]
+        public void SinlgePath()
+        {
+            var sut = new EdgeDisjointPath();
+            var graph = new int[4][];
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                graph[i] = new int[4];
+            }
+
+            graph[0][1] = 1;
+            graph[1][2] = 1;
+            graph[1][3] = 1;
+            graph[2][3] = 1;
+
+            Assert.Equal(1, sut.GetEdgeDisjointPathCount(graph));
+        }
+
+        [Fact]
+        public void BaselineWithZigZagPath()
+        {
+            var sut = new EdgeDisjointPath();
+            var graph = new int[4][];
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                graph[i] = new int[4];
+            }
+
+            graph[0][1] = 1;
+            graph[0][2] = 1;
+            graph[1][2] = 1;
+            graph[1][3] = 1;
+            graph[2][3] = 1;
+
+            Assert.Equal(2, sut.GetEdgeDisjointPathCount(graph));
+        }
+
+        [Fact]
+        public void BaselinWithStraightLines()
+        {
+            var sut = new EdgeDisjointPath();
+            var graph = new int[4][];
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                graph[i] = new int[4];
+            }
+
+            graph[0][1] = 1;
+            graph[0][2] = 1;
+            graph[1][3] = 1;
+            graph[2][3] = 1;
+
+            Assert.Equal(2, sut.GetEdgeDisjointPathCount(graph));
+        }
+
+        [Fact]
+        public void BaselineSmall()
+        {
+            var sut = new EdgeDisjointPath();
+            var graph = new int[6][];
+
+            graph[0] = new[] { 0, 1, 1, 0, 0, 0 };
+            graph[1] = new[] { 0, 0, 1, 1, 0, 0 };
+            graph[2] = new[] { 0, 1, 0, 0, 1, 0 };
+            graph[3] = new[] { 0, 0, 1, 0, 0, 1 };
+            graph[4] = new[] { 0, 0, 0, 1, 0, 1 };
+            graph[5] = new[] { 0, 0, 0, 0, 0, 0 };
+
+            Assert.Equal(2, sut.GetEdgeDisjointPathCount(graph));
+        }
+
+        [Fact]
+        public void Baseline()
+        {
+            var sut = new EdgeDisjointPath();
+            var graph = new int[8][];
+            graph[0]= new [] { 0, 1, 1, 1, 0, 0, 0, 0};
+            graph[1] = new[] { 0, 0, 1, 0, 0, 0, 0, 0 };
+            graph[2] = new[] { 0, 0, 0, 1, 0, 0, 1, 0 };
+            graph[3] = new[] { 0, 0, 0, 0, 0, 0, 1, 0 };
+            graph[4] = new[] { 0, 0, 1, 0, 0, 0, 0, 1 };
+            graph[5] = new[] { 0, 1, 0, 0, 0, 0, 0, 1 };
+            graph[6] = new[] { 0, 0, 0, 0, 0, 1, 0, 1 };
+            graph[7] = new[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            Assert.Equal(2, sut.GetEdgeDisjointPathCount(graph));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/MaximumFlow/EdgeDisjointPath.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph.MaximumFlow
+{
+    public class EdgeDisjointPath
+    {
+        public int GetEdgeDisjointPathCount(int[][] graph)
+        {
+            var residualGraph = new int[graph.Length][];
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                residualGraph[i] = new int[graph.Length];
+
+                for (int j = 0;j < residualGraph[i].Length; j++)
+                {
+                    residualGraph[i][j] = graph[i][j];
+                }
+            }
+
+            const int source = 0;
+            var sink = graph.Length - 1;
+            bool hasPath;
+            var edgeDisjointPathCount = 0;
+
+            do
+            {
+                var path = GetPath(residualGraph, source, sink);
+                hasPath = path != null;
+
+                if (hasPath)
+                {
+                    // We don't need to calculate delta for this kind of problem since all edges has max 1 capacity. 
+                    var delta = 1;
+                    var currentVertice = sink;
+                    var parent = path[sink];
+                    edgeDisjointPathCount += 1;
+                    while (parent >= 0)
+                    {
+                        residualGraph[parent][currentVertice] = residualGraph[parent][currentVertice] - delta;
+                        residualGraph[currentVertice][parent] = residualGraph[currentVertice][parent] + delta;
+                        currentVertice = parent;
+                        parent = path[currentVertice];
+                    }
+                }
+
+            } while (hasPath);
+
+            return edgeDisjointPathCount;
+        }
+
+        private int[] GetPath(int[][] residualGraph, int source, int sink)
+        {
+            var queue = new Queue<int>();
+            var visited = new bool[residualGraph.Length];
+            var path = new int[residualGraph.Length];
+
+            queue.Enqueue(source);
+            visited[source] = true;
+            path[source] = -1;
+
+            while (queue.Count > 0)
+            {
+                var currentVertice = queue.Dequeue();
+
+                for (int i = 0; i < residualGraph[currentVertice].Length; i++)
+                {
+                    if (!visited[i] && residualGraph[currentVertice][i] > 0)
+                    {
+                        path[i] = currentVertice;
+                        visited[i] = true;
+                        queue.Enqueue(i);
+                        
+                        if (i == sink)
+                        {
+                            return path;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
