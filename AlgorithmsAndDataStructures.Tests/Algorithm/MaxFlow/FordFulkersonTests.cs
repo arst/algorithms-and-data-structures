98e79b8c7813127e7cbc138567189ed1c3c785c1
arst <artem.startsev@gmail.com>
2020-02-26 09:42:28 +0200

    Ford Fulkerson(not working)

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/MaxFlow/FordFulkersonTests.cs
@@ -0,0 +1,28 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.MaximumFlow;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.MaxFlow
+{
+    public class FordFulkersonTests
+    {
+        [Fact]
+        public void Baseline()
+        {
+            var sut = new FordFulkerson();
+            var graph = new int[4][];
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                graph[i] = new int[4];
+            }
+
+            graph[0][1] = 3;
+            graph[0][2] = 2;
+            graph[1][2] = 5;
+            graph[1][3] = 2;
+            graph[2][3] = 3;
+
+            Assert.Equal(5,sut.MaxFlow(graph));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/MaximumFlow/FordFulkerson.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph.MaximumFlow
+{
+    public class FordFulkerson
+    {
+        // Actually, flow netwrok here is just a directed graph, though, it may as well be undirected.
+        public int MaxFlow(int[][] flowNetwork)
+        {
+            var residualGraph = new int[flowNetwork.GetLength(0)][];
+            var flows = new int[flowNetwork.GetLength(0)][];
+            var isBackEdge = new bool[flowNetwork.GetLength(0)][];
+
+            for (int i = 0; i < residualGraph.Length; i++)
+            {
+                residualGraph[i] = new int[flowNetwork[i].Length];
+                isBackEdge[i] = new bool[flowNetwork[i].Length];
+                flows[i] = new int[flowNetwork[i].Length];
+
+                for (int j = 0; j < residualGraph[i].Length; j++)
+                {
+                    residualGraph[i][j] = flowNetwork[i][j];
+                }
+            }
+
+
+            var hasPath = false;
+
+            do
+            {
+                hasPath = BFS(residualGraph, 0, flowNetwork.Length - 1);
+                var path = GetPath(residualGraph, 0, flowNetwork.Length - 1);
+                if (path is null)
+                {
+                    break;
+                }
+                var delta = GetDelta(residualGraph, path, flowNetwork.Length - 1);
+                EnrichFlows(flows, delta, path, isBackEdge);
+                RebuildResidualGraph(flowNetwork, flows, residualGraph, isBackEdge);
+            } while (hasPath);
+
+            var maxFlow = 0;
+
+            for (int i = 0; i < flowNetwork.Length; i++)
+            {
+                maxFlow += flowNetwork[0][i];
+            }
+
+            return maxFlow;
+        }
+
+        private void RebuildResidualGraph(int[][] flowNetwork, int[][] flows, int[][] residualGraph, bool[][] isBackEdge)
+        {
+            for (int i = 0; i < flowNetwork.Length; i++)
+            {
+                for (int j = 0; j < flowNetwork.Length; j++)
+                {
+                    int residualCapacity = flowNetwork[i][j] - flows[i][j];
+                    if (residualCapacity > 0)
+                    {
+                        residualGraph[i][j] = residualCapacity;
+
+                    }
+                    if (flows[i][j] > 0)
+                    {
+                        isBackEdge[j][i] = true;
+                        residualGraph[j][i] = flows[i][j];
+                    }
+                }
+            }
+        }
+
+        private void EnrichFlows(int[][] flows, int delta, int[] path, bool[][] isBackEdge)
+        {
+            var currentVertice = flows.Length - 1;
+            var parent = path[currentVertice];
+
+            while (parent >= 0)
+            {
+                var isBack = isBackEdge[parent][currentVertice];
+
+                if (isBack)
+                {
+                    flows[parent][currentVertice] = flows[parent][currentVertice] - delta;
+                }
+                else
+                {
+                    flows[parent][currentVertice] = flows[parent][currentVertice] + delta;
+                }
+
+                currentVertice = parent;
+                parent = path[currentVertice];
+            }
+        }
+
+        private int GetDelta(int[][] residualGraph,int[] path, int targetVertice)
+        {
+            var delta = Int32.MaxValue;
+            var currentVertice = targetVertice;
+            var parent = path[currentVertice];
+
+            while (parent >= 0)
+            {
+                delta = Math.Min(residualGraph[parent][currentVertice], delta);
+                currentVertice = parent;
+                parent = path[currentVertice];
+            }
+
+            return delta;
+        }
+
+        private int[] GetPath(int[][] residualGraph, int currentVertice, int targetVertice)
+        {
+            var path = new int[residualGraph.Length];
+            var queue = new Queue<int>();
+            var visited = new bool[residualGraph.Length];
+            path[currentVertice] = -1;
+            queue.Enqueue(currentVertice);
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                for (int i = 0; i < residualGraph[current].Length; i++)
+                {
+                    int adjacentVerticeCapacity = residualGraph[current][i];
+
+                    if (adjacentVerticeCapacity > 0 && !visited[i])
+                    {
+                        path[i] = current;
+                        visited[i] = true;
+
+                        if (i == targetVertice)
+                            return path;
+
+                        queue.Enqueue(i);
+                    }
+                }
+            }
+
+            return null;
+
+        }
+
+        private bool BFS(int[][] residualGraph, int currentVertice, int targetVertice)
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(currentVertice);
+            var visited = new bool[residualGraph.Length];
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                for (int i = 0; i < residualGraph[current].Length; i++)
+                {
+                    int adjacentVerticeCapacity = residualGraph[current][i];
+
+                    if (adjacentVerticeCapacity > 0 && !visited[i])
+                    {
+
+                        visited[i] = true;
+                        if (i == targetVertice)
+                            return true;
+
+                        queue.Enqueue(i);
+                    }  
+                }
+            }
+
+            return false;
+        }
+    }
+}
