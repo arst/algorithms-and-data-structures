81ffa14a1861418e764ab65a47e8070c753b1ac3
arst <artem.startsev@gmail.com>
2020-03-02 08:18:13 +0200

    mMax bipatrite matching DFS based solution

---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/MaximumFlow/MaximumBirpartiteMatchingDfsBased.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph.MaximumFlow
+{
+    public class MaximumBirpartiteMatchingDfsBased
+    {
+        public int GetMaxMatching(int[][] graph)
+        {
+            var result = 0;
+            var assignment = new int[graph.Length];
+
+            for (int i = 0; i < assignment.Length; i++)
+            {
+                assignment[i] = -1;
+            }
+
+            for (int i = 0; i < graph.Length; i++)
+            {
+                var visited = new bool[graph.Length];
+                if (TryAssign(i, graph, visited, assignment))
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+
+        private bool TryAssign(int currentVertice, int[][] graph, bool[] visited, int[] assignment)
+        {
+            for (int i = 0; i < graph.Length; i++)
+            {
+                if (graph[currentVertice][i] > 0 && !visited[i])
+                {
+                    visited[i] = true;
+                    if (assignment[i] < 0 || TryAssign(assignment[i], graph, visited, assignment))
+                    {
+                        assignment[i] = currentVertice;
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
