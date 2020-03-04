e1a248e8e23434fe8d91363bb0da53033e00052b
arst <artem.startsev@gmail.com>
2020-03-04 08:29:52 +0200

    Greedy algorithm for graph coloring

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/Coloring/GreedyColoringTests.cs
@@ -0,0 +1,40 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Coloring;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph.Coloring
+{
+    public class GreedyColoringTests
+    {
+        [Fact]
+        public void Baseline1()
+        {
+            var sut = new GreedyColoring();
+            var graph = new int[5][];
+
+            graph[0] = new [] { 0, 1, 1, 0, 0 };
+            graph[1] = new[] { 1, 0, 1, 1, 0 };
+            graph[2] = new[] { 1, 1, 0, 1, 0 };
+            graph[3] = new[] { 0, 1, 1, 0, 1 };
+            graph[4] = new[] { 0, 0, 0, 1, 0 };
+
+            Assert.Equal(2, sut.Color(graph));
+        }
+
+        [Fact]
+        public void Baseline2()
+        {
+            var sut = new GreedyColoring();
+            var graph = new int[5][];
+
+            graph[0] = new[] { 0, 1, 1, 0, 0 };
+            graph[1] = new[] { 1, 0, 1, 0, 1 };
+            graph[2] = new[] { 1, 1, 0, 0, 1 };
+            graph[3] = new[] { 0, 0, 0, 0, 1 };
+            graph[4] = new[] { 0, 0, 0, 1, 0 };
+
+            Assert.Equal(2, sut.Color(graph));
+        }
+
+        // Randomized tests is needed
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/Coloring/GreedyColoring.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph.Coloring
+{
+    public class GreedyColoring
+    {
+        public int Color(int[][] graph)
+        {
+            var colors = new int[graph.Length];
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+                colors[i] = -1;
+            }
+
+            Color(graph, colors);
+
+            return colors.Max();
+        }
+
+        private void Color(int[][] graph, int[] colors)
+        {
+            colors[0] = 0;
+
+            for (int i = 1; i < graph.Length; i++)
+            {
+                var minAdjacentColor = int.MaxValue;
+                var maxAdjacentColor = int.MinValue;
+
+                for (int j = 0; j < graph.Length; j++)
+                {
+                    if (graph[i][j] < 1 || colors[j] == -1)
+                    {
+                        continue;
+                    }
+
+                    if (colors[j] > maxAdjacentColor)
+                    {
+                        maxAdjacentColor = colors[j];
+                    }
+
+                    if (colors[j] < minAdjacentColor)
+                    {
+                        minAdjacentColor = colors[j];
+                    }
+                }
+
+                // No colored adjacent veetices, color with lowest available color
+                if (minAdjacentColor == int.MaxValue)
+                {
+                    colors[i] = 0;
+                }
+                else if (minAdjacentColor > 0)
+                {
+                    colors[i] = minAdjacentColor - 1;
+                }
+                else
+                {
+                    colors[i] = maxAdjacentColor + 1;
+                }
+            }
+        }
+    }
+}
