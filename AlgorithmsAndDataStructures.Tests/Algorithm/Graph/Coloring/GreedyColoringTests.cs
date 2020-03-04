6cbbdf6e6f80c1c7626aae0b5151b0653b9ac088
arst <artem.startsev@gmail.com>
2020-03-04 09:57:19 +0200

    Property based test for Greedy Graph Coloring

--- AlgorithmsAndDataStructures.Tests/Algorithm/Graph/Coloring/GreedyColoringTests.cs
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/Coloring/GreedyColoringTests.cs
@@ -1,4 +1,6 @@
 ﻿using AlgorithmsAndDataStructures.Algorithms.Graph.Coloring;
+using System;
+using System.Linq;
 using Xunit;
 
 namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph.Coloring
@@ -35,6 +37,53 @@ namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph.Coloring
             Assert.Equal(2, sut.Color(graph));
         }
 
-        // Randomized tests is needed
+        [Fact]
+        public void PropertyBased()
+        {
+            var sut = new GreedyColoring();
+
+            for (int z = 0; z < 1000; z++)
+            {
+                var r = new Random();
+                var vertices = r.Next(1, 100);
+
+                var graph = new int[vertices][];
+
+                for (int i = 0; i < vertices; i++)
+                {
+                    graph[i] = new int[vertices];
+                }
+
+
+                for (int i = 0; i < vertices; i++)
+                {
+                    for (int j = 0; j < vertices; j++)
+                    {
+                        if (i != j)
+                        {
+                            var edge = r.Next(0, 2);
+
+                            graph[i][j] = edge;
+                            graph[j][i] = edge;
+                        }
+                    }
+                }
+
+                var max = int.MinValue;
+
+                for (int i = 0; i < vertices; i++)
+                {
+                    var current = graph[i].Sum();
+
+                    if (current > max)
+                    {
+                        max = current;
+                    }
+                }
+
+                int colors = sut.Color(graph);
+                Assert.True(max + 1 >= colors);
+            }
+        }
     }
 }
