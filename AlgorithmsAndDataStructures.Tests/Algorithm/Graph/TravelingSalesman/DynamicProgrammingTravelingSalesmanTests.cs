f598cf378ad43bc72f0ef79e4449639f9c57cde6
arst <artem.startsev@gmail.com>
2020-03-10 23:00:45 +0200

    Traveling salesman: Dynamic Programming aproach

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/TravelingSalesman/DynamicProgrammingTravelingSalesmanTests.cs
@@ -0,0 +1,36 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.TravelingSalesman;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph.TravelingSalesman
+{
+    public class DynamicProgrammingTravelingSalesmanTests
+    {
+        [Fact]
+        public void Baseline()
+        {
+            var sut = new DynamicProgrammingTravelingSalesman();
+            var graph = new int[4][];
+
+            graph[0] = new[] { 0, 10, 15, 20 };
+            graph[1] = new[] { 10, 0, 35, 25 };
+            graph[2] = new[] { 15, 35, 0, 30 };
+            graph[3] = new[] { 20, 25, 30, 0 };
+
+            Assert.Equal(80, sut.GetPath(graph));
+        }
+
+        [Fact]
+        public void Baseline2()
+        {
+            var sut = new DynamicProgrammingTravelingSalesman();
+            var graph = new int[4][];
+
+            graph[0] = new[] { 0, 20, 42, 25 };
+            graph[1] = new[] { 20, 0, 30, 34 };
+            graph[2] = new[] { 42, 30, 0, 10 };
+            graph[3] = new[] { 25, 34, 10, 0 };
+
+            Assert.Equal(85, sut.GetPath(graph));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/TravelingSalesman/DynamicProgrammingTravelingSalesman.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph.TravelingSalesman
+{
+    public class DynamicProgrammingTravelingSalesman
+    {
+        public int GetPath(int[][] graph)
+        {
+            var dp = new int[(int)Math.Pow(2, graph.Length)][];
+            var allvisited = (1 << graph.Length) - 1;
+
+            for (int i = 0; i < dp.Length; i++)
+            {
+                dp[i] = new int[graph.Length];
+
+                for (int j = 0; j < dp[i].Length; j++)
+                {
+                    dp[i][j] = -1;
+                }
+            }
+
+            var currentRoute = 1; // 0000000000000001 - starting city marked as visited
+            var currentCity = 0; // city at which we start the travel
+            return Travel(graph, dp, allvisited, currentRoute, currentCity);
+        }
+
+        private int Travel(int[][] graph, int[][] dp, double allvisited, int currentRoute, int currentCity)
+        {
+            if (currentRoute == allvisited)
+            {
+                // If all cities visited we need to return to the starting city.
+                return graph[currentCity][0];
+            }
+
+            if (dp[currentRoute][currentCity] != -1)
+            {
+                // If disatnce is already precalculated we can reuse it.
+                return dp[currentRoute][currentCity];
+            }
+
+            var minDistance = int.MaxValue;
+
+            for (int city = 0; city < graph.Length; city++)
+            {
+                // Check if city is already a part of the route.
+                if ((currentRoute & (1 << city)) == 0)
+                {
+                    int distanceThroughCity = graph[currentCity][city] + Travel(graph, dp, allvisited, currentRoute | (1 << city) /*to ensure that city is included in the route*/, city/*we travel further from the city*/);
+
+                    minDistance = Math.Min(minDistance, distanceThroughCity);
+                }
+            }
+
+            dp[currentRoute][currentCity] = minDistance;
+
+            return minDistance;
+        }
+    }
+}
