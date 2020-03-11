c16a80be553ed84bfd0dab69b7ab1e1ffbcbb60e
arst <artem.startsev@gmail.com>
2020-03-11 12:39:56 +0200

    K Centers approximation algorithm

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/KCenters/KCentersGreedyApproximationTests.cs
@@ -0,0 +1,39 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Graph.KCenters;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph.KCenters
+{
+    public class KCentersGreedyApproximationTests
+    {
+        [Fact]
+        public void BaselineForTwoCenters()
+        {
+            var sut = new KCentersGreedyApproximation();
+            var graph = new int[4][];
+            graph[0] = new int[] { 0, 10, 6, 7 };
+            graph[1] = new int[] { 10, 0, 8, 5 };
+            graph[2] = new int[] { 6, 8, 0, 12 };
+            graph[3] = new int[] { 7, 5, 12, 0 };
+
+            Assert.Collection(sut.GetKCenters(graph, 2), 
+                arg => Assert.Equal(0, arg),
+                arg => Assert.Equal(1, arg));
+        }
+
+        [Fact]
+        public void BaselineForThreeCenters()
+        {
+            var sut = new KCentersGreedyApproximation();
+            var graph = new int[4][];
+            graph[0] = new int[] { 0, 10, 6, 7 };
+            graph[1] = new int[] { 10, 0, 8, 5 };
+            graph[2] = new int[] { 6, 8, 0, 12 };
+            graph[3] = new int[] { 7, 5, 12, 0 };
+
+            Assert.Collection(sut.GetKCenters(graph, 3),
+                arg => Assert.Equal(0, arg),
+                arg => Assert.Equal(1, arg),
+                arg => Assert.Equal(2, arg));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Graph/KCenters/KCentersGreedyApproximation.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Graph.KCenters
+{
+    public class KCentersGreedyApproximation
+    {
+        public int[] GetKCenters(int[][] graph, int centers)
+        {
+            var result = new int[centers];
+            var foundCentersCount = 0;
+            result[foundCentersCount] = 0;
+            foundCentersCount++;
+            
+
+            while (foundCentersCount < result.Length)
+            {
+                var maxDisatanceFromCentersVerticeIndex = -1;
+                var maxDisatanceFromCentersVertice = int.MinValue;
+
+                for (int i = 0; i < graph.Length; i++)
+                {
+                    if (result.Contains(i))
+                    {
+                        continue;
+                    }
+
+                    var minDistance = int.MaxValue;
+                    for (int c = 0; c < foundCentersCount; c++)
+                    {
+                        if (c == i)
+                        {
+                            continue;
+                        }
+
+                        if (graph[c][i] < minDistance)
+                        {
+                            minDistance = graph[c][i];
+                        }
+                    }
+
+                    if (maxDisatanceFromCentersVerticeIndex == -1 || maxDisatanceFromCentersVertice < minDistance)
+                    {
+                        maxDisatanceFromCentersVerticeIndex = i;
+                        maxDisatanceFromCentersVertice = minDistance;
+                    }
+                }
+
+                result[foundCentersCount] = maxDisatanceFromCentersVerticeIndex;
+                foundCentersCount++;
+            }
+
+            return result;
+        }
+    }
+}
