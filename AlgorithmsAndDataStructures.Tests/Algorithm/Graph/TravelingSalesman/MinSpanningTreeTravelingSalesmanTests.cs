07aee4861c5695195fcb4574f5df75e2223f745e
arst <artem.startsev@gmail.com>
2020-03-11 09:03:09 +0200

    Traveling salesman: MST approximation

--- AlgorithmsAndDataStructures.Tests/Algorithm/Graph/TravelingSalesman/MinSpanningTreeTravelingSalesmanTests.cs
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Graph/TravelingSalesman/MinSpanningTreeTravelingSalesmanTests.cs
@@ -16,7 +16,22 @@ namespace AlgorithmsAndDataStructures.Tests.Algorithm.Graph.TravelingSalesman
             graph[2] = new[] { 15, 35, 0, 30 };
             graph[3] = new[] { 20, 25, 30, 0 };
 
-            Assert.Equal(80, sut.Travel(graph));
+            Assert.Equal(95, sut.Travel(graph));
+        }
+
+        [Fact]
+        public void Baseline1()
+        {
+            var sut = new MinSpanningTreeTravelingSalesman();
+            var graph = new int[5][];
+
+            graph[0] = new[] { 0, 30, 25, 10, 22 };
+            graph[1] = new[] { 30, 0, 40, 25, 35 };
+            graph[2] = new[] { 25, 40, 0, 20, 10 };
+            graph[3] = new[] { 10, 25, 20, 0, 15 };
+            graph[4] = new[] { 22, 35, 10, 15, 0 };
+
+            Assert.Equal(105, sut.Travel(graph));
         }
     }
 }
--- AlgorithmsAndDataStructures/Algorithms/Graph/TravelingSalesman/MinSpanningTreeTravelingSalesman.cs
+++ AlgorithmsAndDataStructures/Algorithms/Graph/TravelingSalesman/MinSpanningTreeTravelingSalesman.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgorithmsAndDataStructures.Algorithms.Graph.TravelingSalesman
 {
@@ -7,7 +8,7 @@ namespace AlgorithmsAndDataStructures.Algorithms.Graph.TravelingSalesman
     {
         public int Travel(int[][] graph)
         {
-            var startingCity = 0;
+            const int startingCity = 0;
             var edges = new List<Tuple<int, int, int>>();
             var minimumSpanningTree = new List<Tuple<int, int, int>>();
             var spanningTree = new HashSet<int>();
@@ -15,6 +16,7 @@ namespace AlgorithmsAndDataStructures.Algorithms.Graph.TravelingSalesman
 
             spanningTree.Add(startingCity);
 
+            //collect all edges for Prim's algorithm
             for (int i = 0; i < graph.Length; i++)
             {
                 for (int j = 0; j < graph.Length; j++)
@@ -26,7 +28,7 @@ namespace AlgorithmsAndDataStructures.Algorithms.Graph.TravelingSalesman
                     }
                 }
             }
-
+            // Construct MST using Prim's algorithm
             while (spanningTree.Count < graph.Length)
             {
                 Tuple<int, int, int> minEdge = null;
@@ -45,7 +47,49 @@ namespace AlgorithmsAndDataStructures.Algorithms.Graph.TravelingSalesman
                 spanningTree.Add(minEdge.Item2);
             }
 
-            return 0;
+            //Construct a graph with MST tree edges only
+            var minSpanningTreeGraph = new Tuple<int, List<int>>[graph.Length];
+
+            for (int i = 0; i < minimumSpanningTree.Count; i++)
+            {
+                var edge = minimumSpanningTree[i];
+
+                minSpanningTreeGraph[edge.Item1] = minSpanningTreeGraph[edge.Item1] ?? new Tuple<int, List<int>>(edge.Item1, new List<int>());
+                minSpanningTreeGraph[edge.Item2] = minSpanningTreeGraph[edge.Item2] ?? new Tuple<int, List<int>>(edge.Item2, new List<int>());
+
+                minSpanningTreeGraph[edge.Item1].Item2.Add(edge.Item2);
+                minSpanningTreeGraph[edge.Item2].Item2.Add(edge.Item1);
+            }
+
+            // Make DFS traversal of constructed graph
+            var path = new List<int>();
+            var visited = new bool[graph.Length];
+            DFS(minSpanningTreeGraph, path, visited, startingCity);
+            var weight = 0;
+
+            // Calculate path weight
+            for (int i = 0; i < path.Count - 1; i++)
+            {
+                weight += graph[path[i]][path[i + 1]];
+            }
+
+            // We should return to the starting city 
+            return weight + graph[path.Last()][startingCity];
+        }
+
+        private void DFS(Tuple<int, List<int>>[] minSpanningTreeGraph, List<int> path, bool[] visited, int current)
+        {
+            visited[current] = true;
+            path.Add(current);
+
+            List<int> adjacentVertices = minSpanningTreeGraph[current].Item2;
+            for (int i = 0; i < adjacentVertices.Count; i++)
+            {
+                if (!visited[adjacentVertices[i]])
+                {
+                    DFS(minSpanningTreeGraph, path, visited, adjacentVertices[i]);
+                }
+            }
         }
     }
 }
