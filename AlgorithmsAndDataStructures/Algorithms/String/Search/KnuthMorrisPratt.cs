d3916aa3181449be06df3bd48429e4749718a93b
arst <artem.startsev@gmail.com>
2020-03-17 08:27:31 +0200

    Knuth Morris Pratt: template

---
+++ AlgorithmsAndDataStructures/Algorithms/String/Search/KnuthMorrisPratt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmsAndDataStructures.Algorithms.String.Search
+{
+    public class KnuthMorrisPratt : IStringPatternSearchAlgorithm
+    {
+        public int Search(string input, string pattern)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
