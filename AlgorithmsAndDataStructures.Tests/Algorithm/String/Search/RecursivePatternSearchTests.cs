c0f7020685ebf54ae69aa2ea4b4d85435936dfcd
arst <artem.startsev@gmail.com>
2020-03-17 08:27:07 +0200

    Recursive substring search

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/String/Search/RecursivePatternSearchTests.cs
@@ -0,0 +1,12 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.String.Search;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.String.Search
+{
+    public class RecursivePatternSearchTests : StringSearchAlgorithmBaseTests
+    {
+        protected override IStringPatternSearchAlgorithm GetSystemUnderTest()
+        {
+            return new RecursivePatternSearch();
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/String/Search/RecursivePatternSearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AlgorithmsAndDataStructures.Algorithms.String.Search
+{
+    public class RecursivePatternSearch : IStringPatternSearchAlgorithm
+    {
+        public int Search(string input, string pattern)
+        {
+            return SearchInternal(input, 0, pattern, 0);
+        }
+
+        private int SearchInternal(string input, int inputPosition, string pattern, int patternPosition)
+        {
+            if (input.Length - inputPosition < pattern.Length - patternPosition)
+            {
+                return -1;
+            }
+
+            if (input[inputPosition] == pattern[patternPosition])
+            {
+                if (patternPosition == pattern.Length - 1)
+                {
+                    return inputPosition - pattern.Length + 1;
+                }
+
+                return SearchInternal(input, inputPosition + 1, pattern, patternPosition + 1);
+            }
+            else
+            {
+                return SearchInternal(input, patternPosition != 0 ? inputPosition : inputPosition + 1, pattern, 0);
+            }
+        }
+    }
+}
