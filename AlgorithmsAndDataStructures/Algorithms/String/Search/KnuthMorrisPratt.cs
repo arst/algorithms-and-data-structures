9f07a97bb31d6ab6433203aa7717d1cad7bcbbe8
arst <artem.startsev@gmail.com>
2020-03-17 10:06:35 +0200

    Knuth-Morris-Pratt algorithm for substring search

--- AlgorithmsAndDataStructures/Algorithms/String/Search/KnuthMorrisPratt.cs
+++ AlgorithmsAndDataStructures/Algorithms/String/Search/KnuthMorrisPratt.cs
@@ -6,7 +6,68 @@ namespace AlgorithmsAndDataStructures.Algorithms.String.Search
     {
         public int Search(string input, string pattern)
         {
-            throw new NotImplementedException();
+            var aux = ComputeAuxArray(pattern);
+            var patternIndex = 0;
+            var inputIndex = 0;
+
+            while (inputIndex < input.Length)
+            {
+                if (pattern[patternIndex] == input[inputIndex])
+                {
+                    inputIndex++;
+                    patternIndex++;
+                }
+
+                if (patternIndex == pattern.Length)
+                {
+                    return inputIndex - patternIndex;
+                }
+                else if (inputIndex < input.Length && pattern[patternIndex] != input[inputIndex])
+                {
+                    if (patternIndex != 0)
+                    {
+                        patternIndex = aux[patternIndex - 1];
+                    }
+                    else
+                    {
+                        inputIndex++;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        private int[] ComputeAuxArray(string pattern)
+        {
+            var result = new int[pattern.Length];
+            var i = 1;
+            var suffixLength = 0;
+            result[suffixLength] = 0;
+
+            while (i < result.Length)
+            {
+                if (pattern[i] == pattern[suffixLength])
+                {
+                    suffixLength++;
+                    result[i] = suffixLength;
+                    i++;
+                }
+                else
+                {
+                    if (suffixLength != 0)
+                    {
+                        suffixLength = result[suffixLength - 1];
+                    }
+                    else
+                    {
+                        result[i] = suffixLength;
+                        i++;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
