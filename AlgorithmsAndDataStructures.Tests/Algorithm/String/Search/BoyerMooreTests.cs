83348c5dea0113872aaa403e1a981a01886abfb4
arst <artem.startsev@gmail.com>
2020-03-19 09:13:31 +0200

    boyer Moore simplified

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/String/Search/BoyerMooreTests.cs
@@ -0,0 +1,12 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.String.Search;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.String.Search
+{
+    public class BoyerMooreTests : StringSearchAlgorithmBaseTests
+    {
+        protected override IStringPatternSearchAlgorithm GetSystemUnderTest()
+        {
+            return new BoyerMoore();
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/String/Search/BoyerMoore.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsAndDataStructures.Algorithms.String.Search
+{
+    public class BoyerMoore : IStringPatternSearchAlgorithm
+    {
+        public int Search(string input, string pattern)
+        {
+            var badCharacterHeuristicTable = BuildBadCharacterHeuristicTable(pattern);
+
+            var patternIndex = pattern.Length - 1;
+            var inputIndex = pattern.Length - 1;
+
+            while (inputIndex < input.Length)
+            {
+                bool isMatch = input[inputIndex] == pattern[patternIndex];
+                if (isMatch)
+                {
+                    inputIndex--;
+                    patternIndex--;
+                }
+
+                if (patternIndex < 0)
+                {
+                    return inputIndex + 1;
+                }
+                else if (inputIndex > 0 && !isMatch)
+                {
+                    if (patternIndex == pattern.Length - 1)
+                    {
+                        inputIndex++;
+                    }
+                    else
+                    {
+                        var mismatchedCharacter = input[inputIndex];
+                        patternIndex = pattern.Length - 1;
+                        inputIndex = inputIndex + 
+                            (badCharacterHeuristicTable.ContainsKey(mismatchedCharacter) ? badCharacterHeuristicTable[mismatchedCharacter] : pattern.Length);
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        private Dictionary<char, int> BuildBadCharacterHeuristicTable(string pattern)
+        {
+            var result = new Dictionary<char, int>();
+
+            for (int i = 0; i < pattern.Length - 1; i++)
+            {
+                result[pattern[i]] = pattern.Length - i - 1;
+            }
+
+            return result;
+        }
+    }
+}
