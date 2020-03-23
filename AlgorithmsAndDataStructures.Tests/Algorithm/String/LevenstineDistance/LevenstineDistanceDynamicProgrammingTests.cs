648e4a05bae2f30961e5ca730960237f249e41cf
arst <artem.startsev@gmail.com>
2020-03-23 08:46:49 +0200

    Levenstine distance

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/String/LevenstineDistance/LevenstineDistanceDynamicProgrammingTests.cs
@@ -0,0 +1,65 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.String.LevenstineDistance;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.String.LevenstineDistance
+{
+    public class LevenstineDistanceDynamicProgrammingTests
+    {
+        [Fact]
+        public void EmptyStrings()
+        {
+            var sut = new LevenstineDistanceDynamicProgramming();
+
+            Assert.Equal(0, sut.GetLevenstineDistance("", ""));
+        }
+
+        [Fact]
+        public void OneCharacterString()
+        {
+            var sut = new LevenstineDistanceDynamicProgramming();
+
+            Assert.Equal(1, sut.GetLevenstineDistance("b", "a"));
+        }
+
+        [Fact]
+        public void OneCharacterStringEquals()
+        {
+            var sut = new LevenstineDistanceDynamicProgramming();
+
+            Assert.Equal(0, sut.GetLevenstineDistance("a", "a"));
+        }
+
+        [Fact]
+        public void DifferentLengthStrings()
+        {
+            var sut = new LevenstineDistanceDynamicProgramming();
+
+            Assert.Equal(2, sut.GetLevenstineDistance("big", "gigo"));
+        }
+
+        [Fact]
+        public void DifferentLengthStringsInversed()
+        {
+            var sut = new LevenstineDistanceDynamicProgramming();
+
+            Assert.Equal(2, sut.GetLevenstineDistance("gigo", "big"));
+        }
+
+        [Fact]
+        public void EqualsStrings()
+        {
+            var sut = new LevenstineDistanceDynamicProgramming();
+
+            Assert.Equal(0, sut.GetLevenstineDistance("gigo", "gigo"));
+        }
+
+        [Fact]
+        public void Baseline()
+        {
+            var sut = new LevenstineDistanceDynamicProgramming();
+
+            Assert.Equal(5, sut.GetLevenstineDistance("benyam", "ephrem"));
+            Assert.Equal(3, sut.GetLevenstineDistance("kitten", "sitting"));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/String/LevenstineDistance/LevenstineDistanceDynamicProgramming.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AlgorithmsAndDataStructures.Algorithms.String.LevenstineDistance
+{
+    public class LevenstineDistanceDynamicProgramming
+    {
+        public int GetLevenstineDistance(string left, string right)
+        {
+            var distancesTable = new int[left.Length + 1][];
+            
+            for (int i = 0; i < left.Length + 1; i++)
+            {
+                distancesTable[i] = new int[right.Length + 1]; 
+            }
+
+            for (int i = 0; i < right.Length + 1; i++)
+            {
+                // To transform empty string to any substring of right input we need ar least i insertions
+                distancesTable[0][i] = i;
+            }
+
+            for (int i = 0; i < left.Length + 1; i++)
+            {
+                // To transform empty string to any substring of left input we need ar least i insertions
+                distancesTable[i][0] = i;
+            }
+
+            // i = 1 since we already precalculated distance from e,pty string to both left and right substrings
+            for (int i = 1; i < left.Length + 1; i++)
+            {
+                for (int j = 1; j < right.Length + 1; j++)
+                {
+                    distancesTable[i][j] = Math.Min(
+                        // Replace, note: we need to add one only if current characters are mismatched, i and j are shifted by one since we start out distance table
+                        // with empty string and not with first characters
+                        distancesTable[i - 1][j - 1] + (left[i - 1] != right[j - 1] ? 1 : 0), 
+                        Math.Min(
+                        // Insert
+                        distancesTable[i - 1][j] + 1,
+                        // Delete
+                        distancesTable[i][j - 1] + 1));
+                }
+            }
+
+            return distancesTable[left.Length][right.Length];
+        }
+    }
+}
