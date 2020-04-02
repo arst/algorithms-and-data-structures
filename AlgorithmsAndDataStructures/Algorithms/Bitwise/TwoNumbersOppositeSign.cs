ecf192efce69eb80fc41fd4e7ce3909ee17a5e53
arst <artem.startsev@gmail.com>
2020-04-02 09:18:01 +0300

    Bitwise starting

---
+++ AlgorithmsAndDataStructures/Algorithms/Bitwise/TwoNumbersOppositeSign.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmsAndDataStructures.Algorithms.Bitwise
+{
+    public class TwoNumbersOppositeSign
+    {
+        public bool IsOppositeSign(int x, int y)
+        {
+            return false;
+        }
+    }
+}
