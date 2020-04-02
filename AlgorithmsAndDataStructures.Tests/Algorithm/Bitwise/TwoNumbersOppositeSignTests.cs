e59a9ff1e211d674e7ff556ffead433c3472e3c4
arst <artem.startsev@gmail.com>
2020-04-02 09:40:12 +0300

    Bitwise: check for numbers with hegative signs

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Bitwise/TwoNumbersOppositeSignTests.cs
@@ -0,0 +1,40 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Bitwise;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Bitwise
+{
+    public class TwoNumbersOppositeSignTests
+    {
+        [Fact]
+        public void TheSamePositiveSign()
+        {
+            var sut = new TwoNumbersOppositeSign();
+
+            Assert.False(sut.IsOppositeSign(1,2));
+        }
+
+        [Fact]
+        public void TheSameNegativeSign()
+        {
+            var sut = new TwoNumbersOppositeSign();
+
+            Assert.False(sut.IsOppositeSign(-1, -2));
+        }
+
+        [Fact]
+        public void DifferentSigns()
+        {
+            var sut = new TwoNumbersOppositeSign();
+
+            Assert.True(sut.IsOppositeSign(1, -2));
+        }
+
+        [Fact]
+        public void TwoZeroes()
+        {
+            var sut = new TwoNumbersOppositeSign();
+
+            Assert.False(sut.IsOppositeSign(0, 0));
+        }
+    }
+}
--- AlgorithmsAndDataStructures/Algorithms/Bitwise/TwoNumbersOppositeSign.cs
+++ AlgorithmsAndDataStructures/Algorithms/Bitwise/TwoNumbersOppositeSign.cs
@@ -4,7 +4,10 @@
     {
         public bool IsOppositeSign(int x, int y)
         {
-            return false;
+            // The sign of an integer is the left most bit, since negative number is 2d compliment of a positive number
+            // we can XOR two numbers and if the sign is different it would get 1 in the left most bit 
+            // effetively rendering the result to be hegative number, if both numbers are positiove or negative we would get 0 in the leftmost bit
+            return (x ^ y) < 0;
         }
     }
 }
