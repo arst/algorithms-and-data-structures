68a8058a9ec37b55825557e4bb4d2a2f8b05e4ee
arst <artem.startsev@gmail.com>
2020-04-03 08:59:12 +0300

    Add one to the number without arithmetic operations

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Bitwise/AddOneTests.cs
@@ -0,0 +1,18 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Bitwise;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Bitwise
+{
+    public class AddOneTests
+    {
+        [Fact]
+        public void Baseline() 
+        {
+            var sut = new AddOne();
+            for (int i = -100_000_000; i <= 1_000_000; i++)
+            {
+                Assert.Equal(i+1, sut.Add(i));
+            }
+        }
+    }
+}
