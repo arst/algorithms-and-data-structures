f7d922a867a4fd091ccaaee9547c7ac5b57e178b
arst <artem.startsev@gmail.com>
2020-04-03 10:14:15 +0300

    Bitwise: Power of 4

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Bitwise/IsPowerOfFourTests.cs
@@ -0,0 +1,31 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Bitwise;
+using System;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Bitwise
+{
+    public class IsPowerOfFourTests
+    {
+        [Fact]
+        public void PowersOf4()
+        {
+            var sut = new IsPowerOfFour();
+
+            for (int i = 4; i < 1048576; i *= 4)
+            {
+                Assert.True(sut.IsPower(i));
+            }
+        }
+
+        [Fact]
+        public void Baseline()
+        {
+            var sut = new IsPowerOfFour();
+
+            for (int i = 0; i < 1048576; i++)
+            {
+                Assert.Equal(Math.Log(i, 4) % 1 == 0, sut.IsPower(i));
+            }
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Bitwise/IsPowerOfFour.cs
@@ -0,0 +1,18 @@
+﻿namespace AlgorithmsAndDataStructures.Algorithms.Bitwise
+{
+    public class IsPowerOfFour
+    {
+        //0xAAAAAAAA is 10101010101010101010101010101010 in binary which covers all powers that are not powers of 4
+        private const uint nonOddPowers = 0xAAAAAAAA;
+
+        //Though the simples way would be to rake 4 based log and check for the reminder Math.Log(i, 4) % 1 == 0
+        public bool IsPower(int number)
+        {
+            var isNonZero = number != 0;
+            var isEven = (number & (number - 1)) == 0;
+
+            // we can check that all bits for non-4 powers aren't set
+            return isNonZero && isEven && (number & nonOddPowers) == 0;
+        }
+    }
+}
