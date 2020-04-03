153979363676ea9102b166407a388ed925d68b49
arst <artem.startsev@gmail.com>
2020-04-03 09:36:44 +0300

    Bitwise: Multiply by 3.5

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Bitwise/MuttiplyByThreeAndAHalfTests.cs
@@ -0,0 +1,19 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Bitwise;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Bitwise
+{
+    public class MuttiplyByThreeAndAHalfTests
+    {
+        [Fact]
+        public void BaseLine()
+        {
+            var sut = new MuttiplyByThreeAndAHalf();
+
+            for (int i = 0; i <= 1_000_000; i++)
+            {
+                Assert.Equal((int)(i * 3.5), sut.Multiply(i));
+            }
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Bitwise/MuttiplyByThreeAndAHalf.cs
@@ -0,0 +1,12 @@
+﻿namespace AlgorithmsAndDataStructures.Algorithms.Bitwise
+{
+    public class MuttiplyByThreeAndAHalf
+    {
+        // What is 3.5 of the number? It is 2 * number + number + number / 2
+        // We can achieve this with some bit shifts
+        public int Multiply(int input)
+        {
+            return (input << 1) + input + (input >> 1);
+        }
+    }
+}
