7beeaa354c6be07778b77ceab3e75ebb118aad88
arst <artem.startsev@gmail.com>
2020-04-06 21:19:12 +0300

    Dynamic Programming: Longest Common Subsequence

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/DynamincProgramming/LongestCommonSubsequenceTests.cs
@@ -0,0 +1,34 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.DynamicProgramming;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.DynamincProgramming
+{
+    public class LongestCommonSubsequenceTests
+    {
+
+        [Fact]
+        public void OneCharSubsequence()
+        {
+            var sut = new LongestCommonSubsequence();
+
+            Assert.Equal(1, sut.GetLongestSunsequence("azy", "uzt"));
+        }
+
+        [Fact]
+        public void TwoCharSubsequence()
+        {
+            var sut = new LongestCommonSubsequence();
+
+            Assert.Equal(2, sut.GetLongestSunsequence("aab", "azb"));
+        }
+
+        [Fact]
+        public void Baseline()
+        {
+            var sut = new LongestCommonSubsequence();
+
+            Assert.Equal(3, sut.GetLongestSunsequence("ABCDGH", "AEDFHR"));
+            Assert.Equal(4, sut.GetLongestSunsequence("AGGTAB", "GXTXAYB"));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/DynamicProgramming/LongestCommonSubsequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AlgorithmsAndDataStructures.Algorithms.DynamicProgramming
+{
+    public class LongestCommonSubsequence
+    {
+        public int GetLongestSunsequence(string left, string right)
+        {
+            var dp = new int[left.Length + 1][];
+
+            for (int i = 0; i < dp.Length; i++)
+            {
+                dp[i] = new int[right.Length + 1];
+            }
+
+            for (int i = 1; i <= left.Length; i++)
+            {
+                for (int j = 1; j <= right.Length; j++)
+                {
+                    dp[i][j] = left[i - 1] == right[j - 1] 
+                        ? 1 + dp[i - 1][j - 1] // characters amtched so we can add 1 to the result of the previous sub problem
+                        : Math.Max(
+                            dp[i][j - 1]/*remove one character from the  left string*/, 
+                            dp[i - 1][j]/*remove one character from the right string*/);
+                } 
+            }
+
+            return dp[left.Length][right.Length];
+        }
+    }
+}
