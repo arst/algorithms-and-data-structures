0c683758566b463e478bcbcd451f56c317fce35c
arst <artem.startsev@gmail.com>
2020-04-06 09:10:24 +0300

    Dynamic Programming: Fibonacci numbers

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/DynamincProgramming/FibonacciNumberTests.cs
@@ -0,0 +1,45 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.DynamicProgramming;
+using System;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.DynamincProgramming
+{
+    public class FibonacciNumberTests
+    {
+        [Fact]
+        public void ZeroNumber()
+        {
+            var sut = new FibonacciNumber();
+
+            Assert.Equal(0, sut.GetNumber(0));
+        }
+
+        [Fact]
+        public void FirstNumber()
+        {
+            var sut = new FibonacciNumber();
+
+            Assert.Equal(1, sut.GetNumber(1));
+        }
+
+        [Fact]
+        public void SecondNumber()
+        {
+            var sut = new FibonacciNumber();
+
+            Assert.Equal(1, sut.GetNumber(2));
+        }
+
+        [Fact]
+        public void Baseline()
+        {
+            var sut = new FibonacciNumber();
+            var numbers = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Assert.Equal(numbers[i], sut.GetNumber(i));
+            }
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/DynamicProgramming/FibonacciNumber.cs
@@ -0,0 +1,24 @@
+﻿namespace AlgorithmsAndDataStructures.Algorithms.DynamicProgramming
+{
+    public class FibonacciNumber
+    {
+        public int GetNumber(int number)
+        {
+            if (number == 0)
+            {
+                return number;
+            }
+
+            var dp = new int[number + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                dp[i] = dp[i - 2] + dp[i - 1]; 
+            }
+
+            return dp[number];
+        }
+    }
+}
