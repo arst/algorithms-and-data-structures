1a37ac9e4728181c5555f3c805c2c6e687dde5b0
arst <artem.startsev@gmail.com>
2020-04-11 19:02:16 +0300

    Recursive Binary Search

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Search/BinaryRecursiveTests.cs
@@ -0,0 +1,12 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Search;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Search
+{
+    public class BinaryRecursiveTests : BaseSearchAlgorithmTests
+    {
+        public override ISearchAlgorithm<int> GetSystemUnderTest()
+        {
+            return new BinaryRecursive();
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Search/BinaryRecursive.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Search
+{
+    public class BinaryRecursive : ISearchAlgorithm<int>
+    {
+        public int Search(int[] target, int value)
+        {
+            return SearchInternal(target, 0, target.Length, value);
+        }
+
+        private int SearchInternal(int[] target, int start, int end, int value)
+        {
+            if (start >= end)
+            {
+                return -1;
+            }
+
+            var mid = start + (end - start) / 2;
+
+            if (target[mid] == value)
+            {
+                return mid;
+            }
+
+            if (target[mid] > value)
+            {
+                return SearchInternal(target, start, mid, value);
+            }
+            else
+            {
+                return SearchInternal(target, mid + 1, end, value);
+            }
+        }
+    }
+}
