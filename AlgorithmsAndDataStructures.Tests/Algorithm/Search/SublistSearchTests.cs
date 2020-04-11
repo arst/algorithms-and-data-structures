b86849747ef49879cf4a1100c4a063aa7b738e0e
arst <artem.startsev@gmail.com>
2020-04-11 19:21:51 +0300

    Sublist search

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Search/SublistSearchTests.cs
@@ -0,0 +1,109 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Search;
+using AlgorithmsAndDataStructures.DataStructures.LinkedList;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Search
+{
+    public class SublistSearchTests
+    {
+        [Fact]
+        public void OneNodeList()
+        {
+            var sut = new SublistSearch();
+            var input = new SinglyLinkedList<int>();
+            input.Append(1);
+            var pattern = new SinglyLinkedList<int>();
+            pattern.Append(1);
+
+            Assert.True(sut.IsSubset(input, pattern));
+        }
+
+        [Fact]
+        public void PatternLongerThenInput()
+        {
+            var sut = new SublistSearch();
+            var input = new SinglyLinkedList<int>();
+            input.Append(1);
+            var pattern = new SinglyLinkedList<int>();
+            pattern.Append(1);
+            pattern.Append(2);
+
+            Assert.False(sut.IsSubset(input, pattern));
+        }
+
+        [Fact]
+        public void InputLongerThenPattern()
+        {
+            var sut = new SublistSearch();
+            var input = new SinglyLinkedList<int>();
+            input.Append(1);
+            input.Append(2);
+            var pattern = new SinglyLinkedList<int>();
+            pattern.Append(1);
+
+            Assert.True(sut.IsSubset(input, pattern));
+        }
+
+        [Fact]
+        public void MatchAtTheEnd()
+        {
+            var sut = new SublistSearch();
+            var input = new SinglyLinkedList<int>();
+            input.Append(1);
+            input.Append(2);
+            input.Append(1);
+            input.Append(2);
+            input.Append(3);
+            input.Append(4);
+            var pattern = new SinglyLinkedList<int>();
+            pattern.Append(1);
+            pattern.Append(2);
+            pattern.Append(3);
+            pattern.Append(4);
+
+            Assert.True(sut.IsSubset(input, pattern));
+        }
+
+        [Fact]
+        public void PartialMatch()
+        {
+            var sut = new SublistSearch();
+            var input = new SinglyLinkedList<int>();
+            input.Append(1);
+            input.Append(2);
+            input.Append(2);
+            input.Append(1);
+            input.Append(2);
+            input.Append(3);
+            var pattern = new SinglyLinkedList<int>();
+            pattern.Append(1);
+            pattern.Append(2);
+            pattern.Append(3);
+            pattern.Append(4);
+
+            Assert.False(sut.IsSubset(input, pattern));
+        }
+
+        [Fact]
+        public void MatchInTheMiddle()
+        {
+            var sut = new SublistSearch();
+            var input = new SinglyLinkedList<int>();
+            input.Append(1);
+            input.Append(2);
+            input.Append(1);
+            input.Append(2);
+            input.Append(3);
+            input.Append(4);
+            input.Append(1);
+            input.Append(2);
+            var pattern = new SinglyLinkedList<int>();
+            pattern.Append(1);
+            pattern.Append(2);
+            pattern.Append(3);
+            pattern.Append(4);
+
+            Assert.True(sut.IsSubset(input, pattern));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Search/SublistSearch.cs
@@ -0,0 +1,39 @@
+﻿using AlgorithmsAndDataStructures.DataStructures.LinkedList;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Search
+{
+    public class SublistSearch
+    {
+        /*
+         Time Complexity : O(m*n) 
+         where m is the number of nodes in second list and n in first.
+         */
+        public bool IsSubset(SinglyLinkedList<int> input, SinglyLinkedList<int> pattern)
+        {
+            var patternPointer = pattern.GetHead();
+            var inputPointer = input.GetHead();
+
+            while (inputPointer != null && patternPointer != null)
+            {
+                if (inputPointer.Value != patternPointer.Value)
+                {
+                    if (patternPointer != pattern.GetHead())
+                    {
+                        patternPointer = pattern.GetHead();
+                    }
+                    else
+                    {
+                        inputPointer = inputPointer.Next;
+                    }
+                }
+                else
+                {
+                    patternPointer = patternPointer.Next;
+                    inputPointer = inputPointer.Next;
+                }
+            }
+
+            return patternPointer == null;
+        }
+    }
+}
