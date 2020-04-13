cfbdc85dacbff72bb25cf02bbb028bbf35720a9e
arst <artem.startsev@gmail.com>
2020-04-13 09:34:43 +0300

    Concurrency: Simple non blocking wrapper for queue

---
+++ AlgorithmsAndDataStructures.Tests/DataStructures/Concurrency/SimpleNonBlockingQueueTests.cs
@@ -0,0 +1,89 @@
+﻿using AlgorithmsAndDataStructures.DataStructures.Concurrency;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.DataStructures.Concurrency
+{
+    public class SimpleNonBlockingQueueTests
+    {
+        [Fact]
+        public async Task Baseline()
+        {
+            var sut = new SimpleNonBlockingQueue();
+            var queue1 = new Queue<int>();
+            var queue2 = new Queue<int>();
+            var testSize = 1000000;
+            var taskDelay = Task.Delay(TimeSpan.FromSeconds(60));
+            using var cancelationTokenSource = new CancellationTokenSource();
+
+            var writerTask = new Task(() => Write());
+            var readerTask1 = new Task(() => Read(1, cancelationTokenSource.Token));
+            var readerTask2 = new Task(() => Read(2, cancelationTokenSource.Token));
+            readerTask1.Start();
+            writerTask.Start();
+            readerTask2.Start();
+
+            void Write()
+            {
+                for (int i = 0; i < testSize; i++)
+                {
+                    sut.Enqueue(i);
+                }
+            }
+
+            void Read(int queueNumbe, CancellationToken cancellationToken)
+            {
+                var currentQueue = queueNumbe == 1 ? queue1 : queue2;
+
+                while (currentQueue.Count != testSize)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        throw new OperationCanceledException();
+                    }
+
+                    var isDeueued = sut.TryDequeue(out int deuqeued);
+
+                    if (isDeueued)
+                    {
+                        currentQueue.Enqueue(deuqeued);
+                    }
+                }
+            }
+
+            await Task.WhenAny(writerTask, taskDelay).ConfigureAwait(false);
+            cancelationTokenSource.Cancel();
+
+            Assert.True(queue1.Distinct().Count() == queue1.Count);
+
+            while (queue1.Count > 0)
+            {
+                var smallerDequeued = queue1.TryDequeue(out int smaller);
+                var biggerDequeued = queue1.TryDequeue(out int bigger);
+
+                if (smallerDequeued && biggerDequeued)
+                {
+                    Assert.True(smaller < bigger);
+                }
+
+            }
+
+            Assert.True(queue2.Distinct().Count() == queue2.Count);
+
+            while (queue2.Count > 0)
+            {
+                var smallerDequeued = queue2.TryDequeue(out int smaller);
+                var biggerDequeued = queue2.TryDequeue(out int bigger);
+
+                if (smallerDequeued && biggerDequeued)
+                {
+                    Assert.True(smaller < bigger);
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/DataStructures/Concurrency/SimpleNonBlockingQueue.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace AlgorithmsAndDataStructures.DataStructures.Concurrency
+{
+    public class SimpleNonBlockingQueue
+    {
+        private int size;
+        private Queue<int> queue;
+        private int currentSize;
+        private int locked;
+
+        public SimpleNonBlockingQueue(int size = 8)
+        {
+            this.queue = new Queue<int>();
+            this.currentSize = 0;
+            this.size = size;
+            this.locked = 0;
+        }
+
+        public void Enqueue(int value)
+        {
+            while (true)
+            {
+                if (Interlocked.Exchange(ref locked, 1) == 0)
+                {
+                    if (currentSize >= size)
+                    {
+                        Volatile.Write(ref locked, 0);
+                        continue;
+                    }
+
+                    try
+                    {
+                        queue.Enqueue(value);
+                        currentSize++;
+                        return;
+                    }
+                    finally
+                    {
+                        Volatile.Write(ref locked, 0);
+                    }   
+                }
+            }
+        }
+
+        public bool TryDequeue(out int value)
+        {
+            value = -1;
+
+            while (true)
+            {
+                if (Interlocked.Exchange(ref locked, 1) == 0)
+                {
+                    if (currentSize <= 0)
+                    {
+                        Volatile.Write(ref locked, 0);
+                        return false;
+                    }
+
+                    try
+                    {
+                        value = queue.Dequeue();
+                        currentSize--;
+                    }
+                    finally
+                    {
+                        Volatile.Write(ref locked, 0);
+                    }
+                }
+            }
+        }
+    }
+}
