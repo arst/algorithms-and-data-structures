0434d9c800a272ec1791a68fd5177f22f07eb825
arst <artem.startsev@gmail.com>
2020-04-13 09:34:51 +0300

    Concurrency: Simple blocking wrapper for queue

---
+++ AlgorithmsAndDataStructures.Tests/DataStructures/Concurrency/SimpleBlockingQueueTests.cs
@@ -0,0 +1,84 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Concurrency;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.DataStructures.Concurrency
+{
+    public class SimpleBlockingQueueTests
+    {
+        [Fact]
+        public async Task Baseline()
+        {
+            var sut = new SimpleBlockingQueue();
+            var queue1 = new Queue<int>();
+            var queue2 = new Queue<int>();
+            var testSize = 1000000;
+            var taskDelay = Task.Delay(TimeSpan.FromSeconds(60));
+            using var cancelationTokenSource = new CancellationTokenSource();
+
+            var writerTask = new Task(() => Write());
+            var readerTask1 = new Task(() => Read(1, cancelationTokenSource.Token));
+            var readerTask2 = new Task(() => Read(2, cancelationTokenSource.Token));
+            readerTask1.Start();
+            writerTask.Start();
+            readerTask2.Start();
+
+            void Write()
+            {
+                for (int i = 0; i < testSize; i++)
+                {
+                    sut.Enqueue(i);
+                }
+            }
+
+            void Read(int queueNumbe, CancellationToken cancellationToken)
+            {
+                var currentQueue = queueNumbe == 1 ? queue1 : queue2;
+
+                while (currentQueue.Count != testSize)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        throw new OperationCanceledException();
+                    }
+
+                    currentQueue.Enqueue(sut.Dequeue());
+                }
+            }
+
+            await Task.WhenAny(writerTask, taskDelay).ConfigureAwait(false);
+            cancelationTokenSource.Cancel();
+
+            Assert.True(queue1.Distinct().Count() == queue1.Count);
+
+            while (queue1.Count > 0)
+            {
+                var smallerDequeued = queue1.TryDequeue(out int smaller);
+                var biggerDequeued = queue1.TryDequeue(out int bigger);
+
+                if (smallerDequeued && biggerDequeued)
+                {
+                    Assert.True(smaller < bigger);
+                }
+                
+            }
+
+            Assert.True(queue2.Distinct().Count() == queue2.Count);
+
+            while (queue2.Count > 0)
+            {
+                var smallerDequeued = queue2.TryDequeue(out int smaller);
+                var biggerDequeued = queue2.TryDequeue(out int bigger);
+
+                if (smallerDequeued && biggerDequeued)
+                {
+                    Assert.True(smaller < bigger);
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/DataStructures/Concurrency/SimpleBlockingQueue.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Concurrency
+{
+    public class SimpleBlockingQueue
+    {
+        private int size;
+        private Queue<int> queue;
+        private int currentSize;
+        private object lockObject = new object();
+
+        public SimpleBlockingQueue(int size = 8)
+        {
+            this.queue = new Queue<int>();
+            this.currentSize = 0;
+            this.size = size;
+        }
+
+        public void Enqueue(int value)
+        {
+            try
+            {
+                Monitor.Enter(lockObject);
+
+                while (currentSize >= size)
+                {
+                    Monitor.Wait(lockObject);
+                }
+
+                queue.Enqueue(value);
+                currentSize++;
+                // Why do we need to call PulseAll instead of Pulse?
+                // Consider a situation with two producer threads and one consumer thread all working with a queue of size one. 
+                // It's possible that when an item is added to the queue by one of the producer threads, the other two threads are blocked waiting on the condition variable. 
+                // If the producer thread after adding an item invokes Pulse() it is possible that the other producer thread is chosen by the system to resume execution. 
+                // The woken-up producer thread would find the queue full and go back to waiting on the condition variable, causing a deadlock. 
+                // Invoking PulesAll() assures that the consumer thread also gets a chance to wake up and resume execution.
+                Monitor.PulseAll(lockObject);
+            }
+            finally
+            {
+                Monitor.Exit(lockObject);
+            }
+        }
+
+        public int Dequeue()
+        {
+            int dequeued;
+
+            try
+            {
+                Monitor.Enter(lockObject);
+                while (currentSize <= 0)
+                {
+                    Monitor.Wait(lockObject);
+                }
+
+                dequeued = queue.Dequeue();
+                currentSize--;
+                // Why do we need to call PulseAll instead of Pulse?
+                // Consider a situation with two producer threads and one consumer thread all working with a queue of size one. 
+                // It's possible that when an item is added to the queue by one of the producer threads, the other two threads are blocked waiting on the condition variable. 
+                // If the producer thread after adding an item invokes Pulse() it is possible that the other producer thread is chosen by the system to resume execution. 
+                // The woken-up producer thread would find the queue full and go back to waiting on the condition variable, causing a deadlock. 
+                // Invoking PulesAll() assures that the consumer thread also gets a chance to wake up and resume execution.
+                Monitor.PulseAll(lockObject);
+            }
+            finally
+            {
+                Monitor.Exit(lockObject);
+            }
+
+            return dequeued;
+        }
+    }
+}
