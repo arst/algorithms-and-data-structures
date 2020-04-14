6c98f9bd3aec261538ff6fdaa03c0d6ddf18d9ca
arst <artem.startsev@gmail.com>
2020-04-14 09:06:21 +0300

    Leaky bucket: Step 1

---
+++ AlgorithmsAndDataStructures/DataStructures/Concurrency/SimpleLeakyBucket.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace AlgorithmsAndDataStructures.DataStructures.Concurrency
+{
+    public class SimpleLeakyBucket
+    {
+        private int refillRate;
+        private int maxCapacity;
+        private volatile int currentSize;
+        private Queue<int> queue;
+        private int locked;
+
+        public SimpleLeakyBucket(int bucketSize, int maxCapacity, int refillRate)
+        {
+            this.refillRate = refillRate;
+            this.maxCapacity = maxCapacity;
+            currentSize = maxCapacity;
+            locked = 0;
+            queue = new Queue<int>();
+        }
+
+        public void TryEnqueue()
+        {
+            while (true)
+            {
+                if (Interlocked.Exchange(ref locked, 1) == 0)
+                {
+                    if (currentSize >= size)
+                    {
+                        Volatile.Write(ref locked, 0);
+                        continue;
+                    }
+
+                    try
+                    {
+                        queue.Enqueue(value);
+                        currentSize++;
+                        return;
+                    }
+                    finally
+                    {
+                        Volatile.Write(ref locked, 0);
+                    }
+                }
+            }
+        }
+
+        private void Consume()
+        {
+            while (true)
+            {
+                if (Interlocked.Exchange(ref locked, 1) == 0)
+                {
+                    try
+                    {
+                        
+                    }
+                    finally
+                    {
+                        Volatile.Write(ref locked, 0);
+                    }
+                }
+            }
+        }
+    }
+}
