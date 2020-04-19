6cefe8c842ed5052fb9a0779c8a955f7ba88f27a
arst <artem.startsev@gmail.com>
2020-04-19 10:35:35 +0300

    Concurrency: Asynchronous to Synchronous problem

---
+++ AlgorithmsAndDataStructures.Tests/DataStructures/Concurrency/AsynchronousToSynchronousTests.cs
@@ -0,0 +1,23 @@
+﻿using AlgorithmsAndDataStructures.DataStructures.Concurrency;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.DataStructures.Concurrency
+{
+    public class AsynchronousToSynchronousTests
+    {
+        [Fact]
+        public void ExecutesSyncronously()
+        {
+            var sut = new AsynchronousToSynchronous();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                var queue = new Queue<int>();
+                sut.ExecuteSync(queue);
+                Assert.Equal(1, queue.Dequeue());
+                Assert.Equal(2, queue.Dequeue());
+            }
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/DataStructures/Concurrency/AsynchronousToSynchronous.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace AlgorithmsAndDataStructures.DataStructures.Concurrency
+{
+    public class AsynchronousToSynchronous
+    {
+        private class AsyncExecutor
+        {
+            public void ExecuteAsync(Action callback)
+            {
+                ThreadPool.QueueUserWorkItem((object _) => {
+                    Thread.Sleep(1);
+                    callback();
+                });
+            }
+        }
+
+        public void ExecuteSync(Queue<int> queue)
+        {
+            var asyncExecutor = new AsyncExecutor();
+            using var resetEvent = new AutoResetEvent(false);
+
+            asyncExecutor.ExecuteAsync(() => {
+                queue.Enqueue(1);
+                resetEvent.Set();
+            });
+
+            resetEvent.WaitOne();
+            queue.Enqueue(2);
+        }
+    }
+}
