ae840b899b099c90dc577b1e7785e607b377121e
arst <artem.startsev@gmail.com>
2020-04-22 08:53:39 +0300

    Ternary Trie

---
+++ AlgorithmsAndDataStructures/DataStructures/Trie/TernaryTrieNode.cs
@@ -0,0 +1,15 @@
+﻿namespace AlgorithmsAndDataStructures.DataStructures.Trie
+{
+    public class TernaryTrieNode
+    {
+        public char Value { get; set; }
+
+        public TernaryTrieNode GreaterThanCurrentCharacter { get; set; }
+
+        public TernaryTrieNode EqualToCurrentCharacter { get; set; }
+
+        public TernaryTrieNode LessThanCurrentCharacter { get; set; }
+
+        public bool IsWord { get; set; }
+    }
+}
