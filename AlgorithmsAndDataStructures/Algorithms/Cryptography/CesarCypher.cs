84c4a39076d045b36d072f6e71b63ff0e12a7419
arst <artem.startsev@gmail.com>
2020-07-16 21:57:52 +0300

    Cesar Cypher: just fo fun, it's not usefull at all taking into account nowadays comptational power

---
+++ AlgorithmsAndDataStructures/Algorithms/Cryptography/CesarCypher.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Cryptography
+{
+    public class CesarCypher
+    {
+        public string Encode(string input, byte padding)
+        {
+            var resultBuilder = new StringBuilder(input.Length);
+
+            foreach (var symbol in input)
+            {
+                resultBuilder.Append((char)((byte)symbol + padding));
+            }
+
+            return resultBuilder.ToString();
+        }
+
+        public string Decode(string input, byte padding)
+        {
+            var resultBuilder = new StringBuilder(input.Length);
+
+            foreach (var symbol in input)
+            {
+                resultBuilder.Append((char)((byte)symbol - padding));
+            }
+
+            return resultBuilder.ToString();
+        }
+    }
+}
