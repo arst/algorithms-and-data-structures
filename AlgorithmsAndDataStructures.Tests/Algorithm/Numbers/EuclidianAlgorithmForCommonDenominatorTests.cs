27449014515a0a7e7548866c256846382db88e07
arst <artem.startsev@gmail.com>
2020-07-17 09:11:50 +0300

    Euclidian algorithm for common denominator

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Numbers/EuclidianAlgorithmForCommonDenominatorTests.cs
@@ -0,0 +1,25 @@
+﻿using AlgorithmsAndDataStructures.Algorithms;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Numbers
+{
+    public class EuclidianAlgorithmForCommonDenominatorTests
+    {
+        [Fact]
+        public void Base()
+        {
+            var sut = new EuclidianAlgorithmForCommonDenominator();
+
+            Assert.Equal(16, sut.CommnDenominator(32, 48));
+            Assert.Equal(16, sut.CommnDenominator(160, 144));
+        }
+
+        [Fact]
+        public void NoCommonDenomintatorExcept1()
+        {
+            var sut = new EuclidianAlgorithmForCommonDenominator();
+
+            Assert.Equal(1, sut.CommnDenominator(137, 54));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Numbers/EuclidianAlgorithmForCommonDenominator.cs
@@ -0,0 +1,15 @@
+﻿namespace AlgorithmsAndDataStructures.Algorithms
+{
+    public class EuclidianAlgorithmForCommonDenominator
+    {
+        public int CommnDenominator(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+
+            return CommnDenominator(b, a % b);
+        }
+    }
+}
