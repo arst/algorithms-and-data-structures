7373cdf3bef3ef4579335ce7f82c3fcf79840729
arst <artem.startsev@gmail.com>
2020-07-21 08:24:09 +0300

    Fowler Noll Vo 1a-based hasing function

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Hashing/FowlerNollVo1aBasedHashTests.cs
@@ -0,0 +1,18 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Hashing;
+using Microsoft.VisualBasic;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Hashing
+{
+    public class FowlerNollVo1aBasedHashTests
+    {
+        [Fact]
+        public void DifferentHashForDifferentSeedValue()
+        {
+            var sut = new FowlerNollVo1aBasedHash();
+            const string input = "test";
+
+            Assert.NotEqual(sut.GetHash(input, 1), sut.GetHash(input, 2));
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Hashing/FowlerNollVo1aBasedHash.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgorithmsAndDataStructures.Algorithms.Hashing
+{
+    public class FowlerNollVo1aBasedHash
+    {
+        // This is a good function to be used in bloom filters cause it allows us to create different hash functions by just changing the value of the seed.
+        public int GetHash(string input, int seed)
+        {
+            const uint h = 0x811C9DC5;
+            const int p = 0x01000193;
+
+            var z = h ^ seed;
+
+            foreach (var symbol in input)
+            {
+                z = z ^ symbol;
+                z = z * p & 0xFFFFFFFF;
+            }
+
+            return (int)z;
+        }
+    }
+}
