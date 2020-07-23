def3200870a0282dc7c21770aceb13d0720d1224
arst <artem.startsev@gmail.com>
2020-07-23 22:14:22 +0300

    Banzhaf algorithm to measure voting power

---
+++ AlgorithmsAndDataStructures/Algorithms/Misc/BanzhafMeasure/BanzhafMeasure.cs
@@ -0,0 +1,35 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Sampling;
+using System.Collections.Generic;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Misc.BanzhafMeasure
+{
+    public class BanzhafMeasure
+    {
+        public float Measure(string voterName, string[] otherVoters, int quota, Dictionary<string, int> voterWeights, int numberOfTries)
+        {
+            var currentSample = 0;
+            var subsetSelector = new SimpleRandomSubsetGenerator();
+            var numberOfCriticalCoalitions = 0;
+
+            while (currentSample < numberOfTries)
+            {
+                var coalition = subsetSelector.GetRandomSubset(otherVoters);
+                var votes = 0;
+
+                foreach (var voter in coalition)
+                {
+                    votes += voterWeights[voter];
+                }
+
+                if (votes < quota && votes + voterWeights[voterName] >= quota)
+                {
+                    numberOfCriticalCoalitions += 1;
+                }
+
+                currentSample++;
+            }
+
+            return (float)numberOfCriticalCoalitions / currentSample;
+        }
+    }
+}
