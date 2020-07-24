17bd296c27131c20f6960372092d4fd337736c53
arst <artem.startsev@gmail.com>
2020-07-24 09:06:17 +0300

    Sieve of Eratosthenes for prime numbers

---
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Numbers/SieveOfEratosthenesTests.cs
@@ -0,0 +1,27 @@
+﻿using AlgorithmsAndDataStructures.Algorithms.Numbers;
+using System.Linq;
+using Xunit;
+
+namespace AlgorithmsAndDataStructures.Tests.Algorithm.Numbers
+{
+    public class SieveOfEratosthenesTests
+    {
+        [Fact]
+        public void Base()
+        {
+            var sut = new SieveOfEratosthenes();
+            const int input = 31;
+            var primesUpTo31 = new int[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 };
+
+            var result = sut.IsPrime(input);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (primesUpTo31.Contains(i))
+                {
+                    Assert.True(result[i]);
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Algorithms/Numbers/SieveOfEratosthenes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Numbers
+{
+    public class SieveOfEratosthenes
+    {
+        public bool[] IsPrime(int n)
+        {
+            var result = new bool[n + 1];
+
+            // since 0 and 1 aren't considered as prime numbers, we omit them
+            for (int i = 2; i < result.Length; i++)
+            {
+                result[i] = true;
+            }
+
+            // We use i * i instead of i < Math.Sqrt(n) cause power is generally quicker then sqrt.
+            
+            for (int i = 2; i * i < n; i++)
+            {
+                if (result[i] == true)
+                {
+                    for (int j = i; j <= n / i; j++)
+                    {
+                        result[j * i] = false;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
