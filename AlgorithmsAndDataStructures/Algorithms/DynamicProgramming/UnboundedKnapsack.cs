b33f0c2b4910c4e88642cd15a95ac1e8e9f9dc31
arst <artem.startsev@gmail.com>
2020-08-07 21:10:51 +0300

    Dynamic Programming: Unbounded Knapsack: cleanup

--- AlgorithmsAndDataStructures/Algorithms/DynamicProgramming/UnboundedKnapsack.cs
+++ AlgorithmsAndDataStructures/Algorithms/DynamicProgramming/UnboundedKnapsack.cs
@@ -4,13 +4,20 @@ namespace AlgorithmsAndDataStructures.Algorithms.DynamicProgramming
 {
     public class UnboundedKnapsack
     {
+#pragma warning disable CA1822 // Mark members as static
         public int GetMaxGain(int[] weights, int[] values, int knapsackSize)
+#pragma warning restore CA1822 // Mark members as static
         {
+            if (weights is null || values is null)
+            {
+                return default;
+            }
+
             var dp = new int[knapsackSize + 1];
 
-            for (int i = 0; i < dp.Length; i++)
+            for (var i = 0; i < dp.Length; i++)
             {
-                for (int j = 0; j < weights.Length; j++)
+                for (var j = 0; j < weights.Length; j++)
                 {
                     if (weights[j] <= i)
                     {
