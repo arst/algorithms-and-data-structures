f40ffd5d095474e3849e5ce1fb846744e0928d77
arst <artem.startsev@gmail.com>
2020-08-07 20:04:51 +0300

    Cryptography: Ceasar Cipher : cleanup

---
+++ AlgorithmsAndDataStructures/Algorithms/Cryptography/CesarCipher.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace AlgorithmsAndDataStructures.Algorithms.Cryptography
+{
+    public class CesarCipher
+    {
+#pragma warning disable CA1822 // Mark members as static
+        public string Encode(string input, byte padding)
+#pragma warning restore CA1822 // Mark members as static
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+
+            var resultBuilder = new StringBuilder(input.Length);
+
+            foreach (var symbol in input)
+            {
+                resultBuilder.Append((char)((byte)symbol + padding));
+            }
+
+            return resultBuilder.ToString();
+        }
+
+#pragma warning disable CA1822 // Mark members as static
+        public string Decode(string input, byte padding)
+#pragma warning restore CA1822 // Mark members as static
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+
+            var resultBuilder = new StringBuilder(input.Length);
+
+            foreach (var symbol in input)
+            {
+                resultBuilder.Append((char)((byte)symbol - padding));
+            }
+
+            return resultBuilder.ToString();
+        }
+    }
+}
