566223619ec733f32940413431536b2fa5946090
arst <artem.startsev@gmail.com>
2020-08-11 21:10:01 +0300

    Fix flucky test for concurrent queue

--- AlgorithmsAndDataStructures.Tests/DataStructures/Concurrency/SimpleBlockingQueueTests.cs
+++ AlgorithmsAndDataStructures.Tests/DataStructures/Concurrency/SimpleBlockingQueueTests.cs
@@ -18,15 +18,14 @@ namespace AlgorithmsAndDataStructures.Tests.DataStructures.Concurrency
             var queue1 = new Queue<int>();
             var queue2 = new Queue<int>();
             const int testSize = 1000000;
-            var taskDelay = Task.Delay(TimeSpan.FromSeconds(60));
 #pragma warning disable HAA0302 // Display class allocation to capture closure
-            using var cancellationTokenSource = new CancellationTokenSource();
+            
 #pragma warning restore HAA0302 // Display class allocation to capture closure
 
 #pragma warning disable HAA0301 // Closure Allocation Source
             var writerTask = new Task(Write);
-            var readerTask1 = new Task(() => Read(1, cancellationTokenSource.Token));
-            var readerTask2 = new Task(() => Read(2, cancellationTokenSource.Token));
+            var readerTask1 = new Task(() => Read(1));
+            var readerTask2 = new Task(() => Read(2));
 #pragma warning restore HAA0301 // Closure Allocation Source
             readerTask1.Start();
             writerTask.Start();
@@ -36,29 +35,39 @@ namespace AlgorithmsAndDataStructures.Tests.DataStructures.Concurrency
             {
                 for (var i = 0; i < testSize; i++)
                 {
-                    sut.Enqueue(i);
+                    using var cancellationTokenSource = new CancellationTokenSource();
+                    cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(5));
+                    sut.Enqueue(i, TimeSpan.FromSeconds(5), cancellationTokenSource.Token);
                 }
             }
 
-            void Read(int queueNumber, CancellationToken cancellationToken)
+            void Read(int queueNumber)
             {
                 var currentQueue = queueNumber == 1 ? queue1 : queue2;
 
-                while (currentQueue.Count != testSize)
+                while (queue1.Count + queue2.Count != testSize)
                 {
-                    if (cancellationToken.IsCancellationRequested)
+                    using var cancellationTokenSource = new CancellationTokenSource();
+                    cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(5));
+                    int toEnqueue;
+
+                    try
                     {
-                        throw new OperationCanceledException();
+                        toEnqueue = sut.Dequeue(TimeSpan.FromSeconds(5), cancellationTokenSource.Token);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        return;
                     }
 
-                    currentQueue.Enqueue(sut.Dequeue());
+                    currentQueue.Enqueue(toEnqueue);
                 }
             }
 
 #pragma warning disable HAA0101 // Array allocation for params parameter
-            _ = await Task.WhenAny(writerTask, taskDelay).ConfigureAwait(false);
+            await writerTask.ConfigureAwait(false);
+            await Task.WhenAll(readerTask1, readerTask2).ConfigureAwait(false);
 #pragma warning restore HAA0101 // Array allocation for params parameter
-            cancellationTokenSource.Cancel();
 
             Assert.True(!queue1.Intersect(queue2).ToList().Any());
 
