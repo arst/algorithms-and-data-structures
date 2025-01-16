0852fe1b355e3e937e63803162305ead064c8d5c
arst <artem.startsev@gmail.com>
2025-01-16 20:18:15 +0100

    Add Cuckoo filter

--- AlgorithmsAndDataStructures.Tests/DataStructures/CuckooFilters/CuckooFilterTests.cs
+++ AlgorithmsAndDataStructures.Tests/DataStructures/CuckooFilters/CuckooFilterTests.cs
@@ -1,6 +1,92 @@
-namespace AlgorithmsAndDataStructures.Tests.DataStructures.CuckooFilters;
+using System;
+using AlgorithmsAndDataStructures.DataStructures.CuckooFilter;
+using Xunit;
 
-public class CuckooFilterTests
+namespace AlgorithmsAndDataStructures.Tests.DataStructures.CuckooFilters
 {
-    
+    public class CuckooFilterTests
+    {
+        [Fact]
+        public void AddAddsElementToFilter()
+        {
+            var sut = new CuckooFilter(10);
+            sut.Add("test");
+
+            Assert.True(sut.Contains("test"));
+        }
+
+        [Fact]
+        public void ContainsReturnsFalseForNonExistentElement()
+        {
+            var sut = new CuckooFilter(10);
+            Assert.False(sut.Contains("nonexistent"));
+        }
+
+        [Fact]
+        public void ContainsReturnsTrueForAddedElement()
+        {
+            var sut = new CuckooFilter(10);
+            sut.Add("test");
+
+            Assert.True(sut.Contains("test"));
+        }
+
+        [Fact]
+        public void RemoveDeletesElementFromFilter()
+        {
+            var sut = new CuckooFilter(10);
+            sut.Add("test");
+            sut.Remove("test");
+
+            Assert.False(sut.Contains("test"));
+        }
+
+        [Fact]
+        public void RemoveDoesNotAffectNonMatchingElements()
+        {
+            var sut = new CuckooFilter(10);
+            sut.Add("test1");
+            sut.Add("test2");
+
+            sut.Remove("test1");
+
+            Assert.False(sut.Contains("test1"));
+            Assert.True(sut.Contains("test2"));
+        }
+
+        [Fact]
+        public void AddFailsAfterMaxInsertionAttempts()
+        {
+            var sut = new CuckooFilter(1, bucketSize: 1, maxInsertionAttempts: 2);
+
+            sut.Add("test1");
+            Assert.Throws<InvalidOperationException>(() => sut.Add("test2"));
+        }
+
+        [Fact]
+        public void FilterHandlesMultipleBucketsCorrectly()
+        {
+            var sut = new CuckooFilter(5);
+
+            sut.Add("test1");
+            sut.Add("test2");
+            sut.Add("test3");
+
+            Assert.True(sut.Contains("test1"));
+            Assert.True(sut.Contains("test2"));
+            Assert.True(sut.Contains("test3"));
+        }
+
+        [Fact]
+        public void FalsePositivesCanOccur()
+        {
+            var sut = new CuckooFilter(10, fingerprintSize: 4);
+
+            sut.Add("test1");
+            sut.Add("test2");
+
+            // Due to small fingerprint size, this could result in a false positive
+            Assert.True(sut.Contains("nonexistent") || !sut.Contains("nonexistent"));
+        }
+    }
 }
--- AlgorithmsAndDataStructures/DataStructures/CuckooFilter/CuckooFilter.cs
+++ AlgorithmsAndDataStructures/DataStructures/CuckooFilter/CuckooFilter.cs
@@ -1,6 +1,118 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmsAndDataStructures.Algorithms.Hashing;
+
 namespace AlgorithmsAndDataStructures.DataStructures.CuckooFilter;
 
 public class CuckooFilter
 {
+    private readonly int seed;
+    private readonly int fingerprintSize;
+    private readonly int bucketSize;
+    private readonly int maxInsertionAttempts;
+    private readonly List<HashSet<int>> buckets;
+    private readonly FowlerNollVo1ABasedHash hashGenerator;
     
+    private static readonly Random Random = new Random(500);
+    
+    public CuckooFilter(int size, int bucketSize = 100, int seed = 255, int fingerprintSize = 16, int maxInsertionAttempts = 500)
+    {
+        this.bucketSize = bucketSize;
+        this.maxInsertionAttempts = maxInsertionAttempts;
+        this.seed = seed;
+        this.fingerprintSize = fingerprintSize;
+        buckets = new List<HashSet<int>>(size);
+        for (var i = 0; i < size; i++)
+        {
+            buckets.Add(new HashSet<int>(bucketSize));
+        }
+
+        hashGenerator = new FowlerNollVo1ABasedHash();
+    }
+
+    public void Add(string element)
+    {
+        var (fingerPrint, firstBucket, secondBucket) = CalculateBucketPlacement(element);
+        TryInsert(fingerPrint, firstBucket, secondBucket, maxInsertionAttempts);
+    }
+
+    private (int FingerPrint, int FirstBucket, int SecondBucket) CalculateBucketPlacement(string element)
+    {
+        var fingerPrint = CalculateFingerPrint(element);
+        var (firstBucket, secondBucket) = CalculateBuckets(fingerPrint);
+        return (fingerPrint, firstBucket, secondBucket);
+    }
+
+    private int CalculateFingerPrint(string element)
+    {
+        // Take first fingerprintSize bits of the hash
+        return hashGenerator.GetHash(element, seed) & ((1 << fingerprintSize) - 1);
+    }
+
+    private (int FirstBucket, int SecondBucket) CalculateBuckets(int fingerPrint)
+    {
+        var firstBucket = Math.Abs(fingerPrint % buckets.Count);
+        // XOR Makes second bucket calculation cheap and independent of the first bucket
+        var secondBucket = Math.Abs((firstBucket ^ fingerPrint) % buckets.Count);
+        return (firstBucket, secondBucket);
+    }
+
+    private void TryInsert(int fingerPrint, int firstBucket, int secondBucket, int insertAttemptsLeft)
+    {
+        while (true)
+        {
+            if (insertAttemptsLeft == 0)
+            {
+                throw new InvalidOperationException("Failed to insert element. Filter is Full. No dynamic expansion is implemented yet.");
+            }
+
+            if (buckets[firstBucket].Count < bucketSize)
+            {
+                buckets[firstBucket].Add(fingerPrint);
+                return;
+            }
+
+            if (buckets[secondBucket].Count < bucketSize)
+            {
+                buckets[secondBucket].Add(fingerPrint);
+                return;
+            }
+
+            var choose = Random.Next(2);
+            var bucketToEvict = choose == 0 ? firstBucket : secondBucket;
+            var evictionVictim = Random.Next(buckets[bucketToEvict].Count);
+            var evictedFingerPrint = buckets[bucketToEvict].ElementAt(evictionVictim);
+            var (newFirstBucket, newSecondBucket) = CalculateBuckets(evictedFingerPrint);
+
+            fingerPrint = evictedFingerPrint;
+            firstBucket = newFirstBucket;
+            secondBucket = newSecondBucket;
+            insertAttemptsLeft -= 1;
+        }
+    }
+
+    public bool Contains(string element)
+    {
+        var fingerPrint = CalculateFingerPrint(element);
+        var (firstBucket, secondBucket) = CalculateBuckets(fingerPrint);
+        
+        return buckets[firstBucket].Contains(fingerPrint) || buckets[secondBucket].Contains(fingerPrint);
+    }
+
+    public void Remove(string element)
+    {
+        var fingerPrint = CalculateFingerPrint(element);
+        var (firstBucket, secondBucket) = CalculateBuckets(fingerPrint);
+
+        if (buckets[firstBucket].Contains(fingerPrint))
+        {
+            buckets[firstBucket].Remove(fingerPrint);
+        }
+
+        if (buckets[secondBucket].Contains(fingerPrint))
+        {
+            buckets[secondBucket].Remove(fingerPrint);
+        }
+    }
 }
