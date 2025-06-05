2873ad2066b1fdc672ad42419bb4dbfe33d73165
arst <artem.startsev@gmail.com>
2025-06-05 10:39:04 +0200

    Re-factor Tug of War class

--- AlgorithmsAndDataStructures.Tests/Algorithm/Backtracking/TugOfWarTests.cs
+++ AlgorithmsAndDataStructures.Tests/Algorithm/Backtracking/TugOfWarTests.cs
@@ -1,68 +1,99 @@
-﻿using AlgorithmsAndDataStructures.Algorithms.Backtracking;
+﻿using System;
+using System.Linq;
+using AlgorithmsAndDataStructures.Algorithms.Backtracking;
 using Xunit;
 
 namespace AlgorithmsAndDataStructures.Tests.Algorithm.Backtracking;
 
 public class TugOfWarTests
 {
-    [Fact]
-    public void TwoElementsSetHasTug()
+    private readonly TugOfWar sut;
+
+    public TugOfWarTests()
     {
-        var sut = new TugOfWar();
+        sut = new TugOfWar();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData(new int[0])]
+    public void GetTug_EmptyOrNullSet_ReturnsEmptyArrays(int[] set)
+    {
+        var (left, right) = sut.GetTug(set);
+
+        Assert.Empty(left);
+        Assert.Empty(right);
+    }
+
+    [Fact]
+    public void GetTug_TwoEqualElements_ReturnsEqualSplit()
+    {
         var set = new[] { 2, 2 };
         var (left, right) = sut.GetTug(set);
 
-        Assert.Collection(left, arg => Assert.Equal(2, arg));
-        Assert.Collection(right, arg => Assert.Equal(2, arg));
+        Assert.Single(left);
+        Assert.Single(right);
+        Assert.Equal(2, left[0]);
+        Assert.Equal(2, right[0]);
+        Assert.Equal(left.Sum(), right.Sum());
     }
 
     [Fact]
-    public void FourElementsSetHasTug()
+    public void GetTug_FourElements_ReturnsEqualSumSplit()
     {
-        var sut = new TugOfWar();
         var set = new[] { 1, 2, 3, 4 };
         var (left, right) = sut.GetTug(set);
 
-#pragma warning disable HAA0101 // Array allocation for params parameter
-        Assert.Collection(left, arg => Assert.Equal(1, arg), arg => Assert.Equal(4, arg));
-        Assert.Collection(right, arg => Assert.Equal(2, arg), arg => Assert.Equal(3, arg));
-#pragma warning restore HAA0101 // Array allocation for params parameter
+        Assert.Equal(2, left.Length);
+        Assert.Equal(2, right.Length);
+        Assert.Equal(left.Sum(), right.Sum());
+        Assert.Equal(5, left.Sum());
     }
 
     [Fact]
-    public void OddElementsSetHasTug()
+    public void GetTug_OddElementCount_ReturnsValidSplit()
     {
-        var sut = new TugOfWar();
         var set = new[] { 2, 2, 3, 1, 4 };
         var (left, right) = sut.GetTug(set);
-#pragma warning disable HAA0101 // Array allocation for params parameter
-        Assert.Collection(left, arg => Assert.Equal(2, arg), arg => Assert.Equal(4, arg));
-        Assert.Collection(right, arg => Assert.Equal(2, arg), arg => Assert.Equal(3, arg), arg => Assert.Equal(1, arg));
-#pragma warning restore HAA0101 // Array allocation for params parameter
+
+        Assert.Equal(2, left.Length);
+        Assert.Equal(3, right.Length);
+        Assert.Equal(left.Sum(), right.Sum());
+        Assert.Equal(6, left.Sum());
     }
 
     [Fact]
-    public void BaseLineEven()
+    public void GetTug_LargeEvenSet_ReturnsValidSplit()
     {
-        var sut = new TugOfWar();
         var set = new[] { 3, 4, 5, -3, 100, 1, 89, 54, 23, 20 };
         var (left, right) = sut.GetTug(set);
-#pragma warning disable HAA0101 // Array allocation for params parameter
-        Assert.Collection(left, arg => Assert.Equal(3, arg), arg => Assert.Equal(5, arg), arg => Assert.Equal(-3, arg),
-            arg => Assert.Equal(89, arg), arg => Assert.Equal(54, arg));
-        Assert.Collection(right, arg => Assert.Equal(4, arg), arg => Assert.Equal(100, arg),
-            arg => Assert.Equal(1, arg), arg => Assert.Equal(23, arg), arg => Assert.Equal(20, arg));
-#pragma warning restore HAA0101 // Array allocation for params parameter
+
+        Assert.Equal(5, left.Length);
+        Assert.Equal(5, right.Length);
+        Assert.Equal(left.Sum(), right.Sum());
+        Assert.Equal(148, left.Sum());
     }
 
     [Fact]
-    public void BaseLineOdd()
+    public void GetTug_NoSolutionExists_ReturnsEmptyArrays()
     {
-        var sut = new TugOfWar();
         var set = new[] { 23, 45, -34, 12, 0, 98, -99, 4, 189, -1, 4 };
         var (left, right) = sut.GetTug(set);
 
         Assert.Empty(left);
         Assert.Empty(right);
+    }
+
+    [Theory]
+    [InlineData(new[] { 1, 1, 1, 1 })]       // Equal numbers
+    [InlineData(new[] { -2, -2, 2, 2 })]     // Positive and negative
+    [InlineData(new[] { 0, 0, 0, 0 })]       // All zeros
+    public void GetTug_SymmetricSets_ReturnsValidSplit(int[] set)
+    {
+        var (left, right) = sut.GetTug(set);
+
+        Assert.Equal(2, left.Length);
+        Assert.Equal(2, right.Length);
+        Assert.Equal(left.Sum(), right.Sum());
     }
 }
--- AlgorithmsAndDataStructures/Algorithms/Backtracking/TugOfWar.cs
+++ AlgorithmsAndDataStructures/Algorithms/Backtracking/TugOfWar.cs
@@ -4,68 +4,92 @@ using System.Linq;
 
 namespace AlgorithmsAndDataStructures.Algorithms.Backtracking;
 
+/// <summary>
+/// Solves the Tug of War problem - dividing a set of numbers into two subsets with equal sums
+/// and minimal size difference (at most 1).
+/// </summary>
 public class TugOfWar
 {
-#pragma warning disable CA1822 // Mark members as static
+    /// <summary>
+    /// Finds two subsets with equal sums from the given set.
+    /// </summary>
+    /// <param name="set">Input array of numbers</param>
+    /// <returns>Two arrays representing left and right subsets with equal sums. 
+    /// Returns empty arrays if no solution exists.</returns>
     public (int[] left, int[] right) GetTug(int[] set)
-#pragma warning restore CA1822 // Mark members as static
     {
-        if (set is null)
-            return (Array.Empty<int>(), Array.Empty<int>());
-        var leftTugLength = set.Length % 2 == 1 ? (set.Length - 1) / 2 : set.Length / 2;
-        var visited = new bool[set.Length];
-        var result = new int[leftTugLength];
-        var hasTug = GetLeftTug(set, visited, result, 0);
-        return (hasTug ? result : Array.Empty<int>(),
-            hasTug ? GetRightTug(set, visited, leftTugLength) : Array.Empty<int>());
-    }
-
-    private static bool GetLeftTug(IReadOnlyList<int> set, bool[] included, IList<int> result, int leftTugParticipants)
-    {
-        if (leftTugParticipants >= result.Count)
+        if (set == null || set.Length == 0)
         {
-            var rightTug = GetRightTug(set, included, result.Count);
-            return rightTug.Sum() == result.Sum();
+            return (Array.Empty<int>(), Array.Empty<int>());
         }
 
-        var firstNonIncludedIndex = GetNotIncludedIndex(set, included, 0);
+        // For odd-length sets, left side gets (n-1)/2 elements, right side gets (n+1)/2
+        var leftTugLength = set.Length % 2 == 1 ? (set.Length - 1) / 2 : set.Length / 2;
+        var visited = new bool[set.Length];
+        var leftTug = new int[leftTugLength];
 
-        while (firstNonIncludedIndex != -1)
+        return TryFindTug(set, visited, leftTug)
+            ? (leftTug, GetRightTug(set, visited))
+            : (Array.Empty<int>(), Array.Empty<int>());
+    }
+
+    private static bool TryFindTug(IReadOnlyList<int> set, bool[] included, int[] leftTug, int leftTugParticipants = 0)
+    {
+        // Base case: if we've filled the left tug, check if sums are equal
+        if (leftTugParticipants >= leftTug.Length)
         {
-            result[leftTugParticipants] = set[firstNonIncludedIndex];
-            leftTugParticipants++;
-            included[firstNonIncludedIndex] = true;
-            var hasTug = GetLeftTug(set, included, result, leftTugParticipants);
-            if (hasTug)
+            var rightTug = GetRightTug(set, included);
+            return rightTug.Sum() == leftTug.Sum();
+        }
+
+        // Try each unused number in the left tug
+        var candidateIndex = FindNextCandidate(set, included, 0);
+        while (candidateIndex != -1)
+        {
+            // Include current number in left tug
+            leftTug[leftTugParticipants] = set[candidateIndex];
+            included[candidateIndex] = true;
+
+            // Recursively try to complete the solution
+            if (TryFindTug(set, included, leftTug, leftTugParticipants + 1))
+            {
                 return true;
-            included[firstNonIncludedIndex] = false;
-            leftTugParticipants--;
-            firstNonIncludedIndex = GetNotIncludedIndex(set, included, firstNonIncludedIndex + 1);
+            }
+
+            // Backtrack
+            included[candidateIndex] = false;
+            candidateIndex = FindNextCandidate(set, included, candidateIndex + 1);
         }
 
         return false;
     }
 
-    private static int[] GetRightTug(IReadOnlyList<int> set, IReadOnlyList<bool> included, int leftTugLength)
+    private static int[] GetRightTug(IReadOnlyList<int> set, IReadOnlyList<bool> included)
     {
-        var result = new int[set.Count - leftTugLength];
-        var resultPointer = 0;
+        var rightSize = set.Count - included.Count(x => x);
+        var result = new int[rightSize];
+        var resultIndex = 0;
+
         for (var i = 0; i < set.Count; i++)
+        {
             if (!included[i])
             {
-                result[resultPointer] = set[i];
-                resultPointer++;
+                result[resultIndex++] = set[i];
             }
+        }
 
         return result;
     }
 
-    private static int GetNotIncludedIndex(IReadOnlyCollection<int> set, IReadOnlyList<bool> included,
-        int startingPosition)
+    private static int FindNextCandidate(IReadOnlyCollection<int> set, IReadOnlyList<bool> included, int startFrom)
     {
-        for (var i = startingPosition; i < set.Count; i++)
+        for (var i = startFrom; i < set.Count; i++)
+        {
             if (!included[i])
+            {
                 return i;
+            }
+        }
         return -1;
     }
 }
